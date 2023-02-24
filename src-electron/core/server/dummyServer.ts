89811574fc8995c61b9a3f40ee45f7e2b6e4ca49
CivilTT <civiltt.ss@gmail.com>
2023-02-24 16:22:39 +0900

    💡 Added dev. comments

---
+++ src-electron/core/server/dummyServer.ts
@@ -0,0 +1,10 @@
+// サーバーJARの起動（入出力のチェックができること）
+
+// リモート関連のプログレスバー
+
+// Eulaの同意（失敗した場合などのエラー処理も）
+
+// 二，三個の適当なプロセスもどき
+
+
+// フロントとバックの通信にはElectronのIPCを使う（https://www.electronjs.org/ja/docs/latest/tutorial/ipc）
