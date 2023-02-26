a8f515d5f4fd9498cdced83b98588e632a182644
CivilTT <civiltt.ss@gmail.com>
2023-02-26 20:40:11 +0900

    💄 Created ConsolePage's style

--- src-electron/core/server/dummyServer.ts
+++ src-electron/core/server/dummyServer.ts
@@ -44,6 +44,46 @@ export async function readyDummy(event: Electron.IpcMainInvokeEvent, world:World
     await sleep(1)
 }
 
+
+const demoConsoles = [
+    '[00:15:29] [ServerMain/INFO]: Building unoptimized datafixer',
+    '[00:15:29] [ServerMain/INFO]: Environment: authHost=\'https://authserver.mojang.com\', accountsHost=\'https://api.mojang.com\', sessionHost=\'https://sessionserver.mojang.com\', servicesHost=\'https://api.minecraftservices.com\', name=\'PROD\'',
+    '[00:15:31] [ServerMain/INFO]: Loaded 7 recipes',
+    '[00:15:31] [ServerMain/INFO]: Loaded 1179 advancements',
+    '[00:15:32] [Server thread/INFO]: Starting minecraft server version 1.00.0',
+    '[00:15:32] [Server thread/INFO]: Loading properties',
+    '[00:15:32] [Server thread/INFO]: Default game type: SURVIVAL',
+    '[00:15:32] [Server thread/INFO]: Generating keypair',
+    '[00:15:32] [Server thread/INFO]: Starting Minecraft server on *:25565',
+    '[00:15:32] [Server thread/INFO]: Using default channel type',
+    '[00:15:32] [Server thread/INFO]: Preparing level "worlds/test/world"',
+    '[00:15:33] [Server thread/INFO]: Preparing start region for dimension minecraft:overworld',
+    '[00:15:35] [Worker-Main-1/INFO]: Preparing spawn area: 0%',
+    '[00:15:35] [Worker-Main-1/INFO]: Preparing spawn area: 0%',
+    '[00:15:35] [Worker-Main-5/INFO]: Preparing spawn area: 0%',
+    '[00:15:35] [Worker-Main-3/INFO]: Preparing spawn area: 0%',
+    '[00:15:35] [Worker-Main-6/INFO]: Preparing spawn area: 0%',
+    '[00:15:35] [Worker-Main-3/INFO]: Preparing spawn area: 75%',
+    '[00:15:35] [Server thread/INFO]: Time elapsed: 2616 ms',
+    '[00:15:35] [Server thread/INFO]: Done (3.637s)! For help, type "help"',
+    '[00:15:39] [User Authenticator #1/INFO]: UUID of player CivilTT is 7aa8d952-5617-4a8c-8f4f-8761999a1f1a',
+    '[00:15:39] [Server thread/INFO]: CivilTT[/127.0.0.1:57745] logged in with entity id 163 at (-47.033365867584706, 27.0, -220.4505154045585)',
+    '[00:15:39] [Server thread/INFO]: CivilTT joined the game',
+    '[02:05:35] [Server thread/INFO]: CivilTT lost connection: Disconnected',
+    '[02:05:35] [Server thread/INFO]: CivilTT left the game',
+    '[02:05:41] [Server thread/INFO]: Stopping the server',
+    '[02:05:41] [Server thread/INFO]: Stopping server',
+    '[02:05:41] [Server thread/INFO]: Saving players',
+    '[02:05:41] [Server thread/INFO]: Saving worlds',
+    '[02:05:42] [Server thread/INFO]: Saving chunks for level \'ServerLevel[worlds/Saku/world]\'/minecraft:overworld',
+    '[02:05:43] [Server thread/INFO]: Saving chunks for level \'ServerLevel[worlds/Saku/world]\'/minecraft:the_end',
+    '[02:05:43] [Server thread/INFO]: Saving chunks for level \'ServerLevel[worlds/Saku/world]\'/minecraft:the_nether',
+    '[02:05:43] [Server thread/INFO]: ThreadedAnvilChunkStorage (world): All chunks are saved',
+    '[02:05:43] [Server thread/INFO]: ThreadedAnvilChunkStorage (DIM1): All chunks are saved',
+    '[02:05:43] [Server thread/INFO]: ThreadedAnvilChunkStorage (DIM-1): All chunks are saved',
+    '[02:05:43] [Server thread/INFO]: ThreadedAnvilChunkStorage: All dimensions are saved'
+]
+
 export async function runDummy(event: Electron.IpcMainInvokeEvent, world:World) {
     // TODO: Windowがsend()を受けられる状態になったことを検知する手法があればsleep(0.5)は不要
     await sleep(0.5)
@@ -55,12 +95,10 @@ export async function runDummy(event: Electron.IpcMainInvokeEvent, world:World) 
 
 
     // 表示画面にコンソールの中身を順次転送
-    addConsole('Start')
-    await sleep(2)
-    addConsole('Finished !!')
-    await sleep(2)
-    addConsole('CivilTT is entered the world')
-
+    for (let i = 0; i < demoConsoles.length; i++) {
+        addConsole(demoConsoles[i])
+        await sleep(0.2)
+    }
 
 
     // コマンド
--- src/pages/Console/ConsolePage.vue
+++ src/pages/Console/ConsolePage.vue
@@ -1,19 +1,47 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { addConsole, getStore } from './ConsoleStore';
 
 const items = getStore()
+const command = ref('')
+
+// コンソール表示
 window.ConsoleAPI.onAddConsole((_event, value) => {
   addConsole(value[0])
+  let container = document.getElementById('scroll')
+  container?.scrollIntoView(false)
+  if (container != null) {
+    container.scrollTop = container.scrollHeight
+  }
 })
 </script>
 
 <template>
   <div class="q-pa-md">
-    <q-scroll-area style="height: 80vh; background-color: gray;">
-      <div v-for="item in items.console" :key="item" style="font-size: 90px;">
-        <li>{{ item }}</li>
+    <div id="scroll" class="q-pl-sm q-pt-sm console">
+      <div v-for="item in items.console" :key="item" style="width: max-content;">
+        <p>{{ item }}</p>
       </div>
-    
-    </q-scroll-area>
+    </div>
+    <div class="row q-pt-md">
+      <q-btn color="red" label="stop"/>
+      <q-btn color="green" label="reboot"/>
+      <q-input filled v-model="command" label="Command"/>
+      <q-btn color="primary" label="send"/>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+p {
+  font-size: 16pt;
+  line-height: 1.2;
+  margin: 0;
+}
+
+.console {
+  background-color: lightgray;
+  overflow: scroll;
+  height: calc(90vh - 100px);
+}
+</style>
