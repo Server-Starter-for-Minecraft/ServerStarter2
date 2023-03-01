3a9766cd664cbbf7c351cea561dbfc560b83f104
CivilTT <civiltt.ss@gmail.com>
2023-03-01 23:26:12 +0900

    ✨ Added menu list in MainPage

---
+++ src/components/Main/WorldVue.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps(['avater', 'version', 'worldName'])
+</script>
+
+<template>
+<q-item clickable>
+  <q-item-section avatar>
+    <q-img :src="avater" />
+  </q-item-section>
+  <q-item-section>{{ worldName }}</q-item-section>
+  <q-item-section side>
+    <q-item-label caption>{{ version }}</q-item-label>
+  </q-item-section>
+</q-item>
+</template>
