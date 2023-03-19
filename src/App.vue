17542f45cf103bd3594aa9c3f74bdf0ef0b290a4
CivilTT <civiltt.ss@gmail.com>
2023-03-19 22:51:56 +0900

    💄 Supported for dark mode

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 
 <script setup lang="ts">
 import { addConsole } from './stores/ConsoleStore';
+import { useQuasar } from 'quasar'
+
+// ダークモードを有効にする
+const $q = useQuasar()
+$q.dark.set('auto')
 
 // メッセージをコンソールに追加
 window.ConsoleAPI.onAddConsole((_event, value) => {
