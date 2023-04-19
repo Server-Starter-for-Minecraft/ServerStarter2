192e6693ed74d0fc4363787bcf3cc0fc8692127e
CivilTT <civiltt.ss@gmail.com>
2023-04-20 00:50:30 +0900

    🐛 Fixed height settings in Main page

--- src/components/Main/WorldListVue.vue
+++ src/components/Main/WorldListVue.vue
@@ -3,6 +3,20 @@ import { World, WorldSettings } from 'app/src-electron/api/scheme';
 import worldVue from 'app/src/components/Main/WorldVue.vue';
 import { ref } from 'vue';
 
+// import { ref } from 'vue';
+import { useSystemStore } from 'src/stores/SystemStore';
+import { useMainStore } from 'src/stores/MainStore';
+import iconBtn from '../util/iconButton.vue';
+
+const autoShutdown = ref(true);
+
+const systemStore = useSystemStore();
+
+useMainStore().setHeader('Server Starter for Minecraft', {
+  sideText: `ver ${systemStore.systemVersion}`,
+});
+
+
 /////////////////// demoデータ ///////////////////
 const demoWorldSettings: WorldSettings = {
   avater_path: 'https://cdn.quasar.dev/img/parallax2.jpg',
@@ -25,8 +39,22 @@ const sortTypes = ['名前', '最終プレイ'];
 </script>
 
 <template>
-  <div>
-    <div class="row q-ma-md">
+  <div class="column" style="height: calc(100vh - 80px);">
+
+    
+    <q-item class="col-auto q-pa-md row expandHeader">
+      <q-item-section>
+        <p class="q-pl-md q-pt-lg">IP. {{ systemStore.publicIP }}</p>
+        <q-checkbox v-model="autoShutdown" class="checkbox"
+          >サーバー終了後にPCを自動シャットダウンする</q-checkbox
+        >
+      </q-item-section>
+      <q-item-section side>
+        <icon-btn icon="tune" size="16pt" text="詳細設定" to="settings" />
+      </q-item-section>
+    </q-item>
+
+    <div class="row col-auto q-ma-md">
       <q-btn color="primary" to="new-world">新規作成</q-btn>
   
       <q-input
@@ -66,6 +94,7 @@ const sortTypes = ['名前', '最終プレイ'];
       v-slot="{ item, index }"
       class="col q-pa-sm mainField"
       :items-size="500"
+      style="width: 100%;"
     >
       <world-vue :world="item" :idx="index" />
     </q-virtual-scroll>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -16,15 +16,17 @@ window.API.onStartServer((_: Electron.IpcRendererEvent) => goConsole());
   
   <!-- 230はHeader-vueの高さ -->
   <q-page>
-    <div class="column" style="height: calc(100vh - 80px);">
+    <!-- <header-vue/> -->
+    <world-list-vue/>
+    <!-- <div class="column" style="height: calc(100vh - 80px);">
       <header-vue/>
 
       <div class="col">
         <world-list-vue/>
-        <!-- <template v-for="text in Array(100)" :key="text">
+        <template v-for="text in Array(100)" :key="text">
           a<br/>
-        </template> -->
+        </template>
       </div>
-    </div>
+    </div> -->
   </q-page>
 </template>
