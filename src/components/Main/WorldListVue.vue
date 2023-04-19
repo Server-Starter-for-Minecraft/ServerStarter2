f22549dc76a7365ae3d6a1eb0d5642dfa2d01053
txkodo <kodo.hsmkai@gmail.com>
2023-04-20 01:33:46 +0900

    MinaPageのレイアウトを調整

--- src/components/Main/WorldListVue.vue
+++ src/components/Main/WorldListVue.vue
@@ -8,14 +8,13 @@ import { useMainStore } from 'src/stores/MainStore';
     :items="useMainStore().showWorldList"
     separator
     v-slot="{ item, index }"
-    class="col q-pa-sm mainField"
+    class="q-pa-sm mainField fit"
     :items-size="500"
-    style="width: 100%;"
+    style="flex: 1 1 0"
   >
     <world-vue :world="item" :idx="index" />
   </q-virtual-scroll>
-
-  <div v-show="useMainStore().showWorldList.length == 0" class="col">
-    <p class="q-pa-none">おや？　お探しのワールドは存在しないようです</p>
+  <div v-show="useMainStore().showWorldList.length == 0" class="col row fit justify-center items-center">
+    <p class="q-pa-none">おや？ お探しのワールドは存在しないようです</p>
   </div>
 </template>
--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -18,16 +18,5 @@ window.API.onStartServer((_: Electron.IpcRendererEvent) => goConsole());
     <header-vue/>
     <worldOperation/>
     <worldList/>
-    
-    <!-- <div class="bg-red q-pa-md">BAR-1</div>
-    <div class="bg-green q-pa-md">BAR-2</div>
-    <q-virtual-scroll
-        class="bg-yellow"
-        :items="[0,0,0,0,0,0,0,0,0,0]"
-        v-slot="{ item, index }"
-        style="flex: 1 1 0"
-    >
-      <div class="text-h1">{{item}}{{ index }}</div>
-    </q-virtual-scroll> -->
   </q-page>
 </template>
