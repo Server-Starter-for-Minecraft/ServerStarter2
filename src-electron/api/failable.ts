bc998eb9dc14719312c46146cc90aa35e91af1fa
txkodo <kodo.hsmkai@gmail.com>
2023-04-21 17:14:09 +0900

    ♻️ Failableを修正

--- src-electron/api/failable.ts
+++ src-electron/api/failable.ts
@@ -1,13 +1,9 @@
 export type Failable<S, F extends Error = Error> = S | F;
 
-export function isSuccess<S, F extends Error>(
+export const isSuccess = <S, F extends Error>(
   value: Failable<S, F>
-): value is S {
-  return !(value instanceof Error);
-}
+): value is S => !(value instanceof Error);
 
-export function isFailure<S, F extends Error>(
+export const isFailure = <S, F extends Error>(
   value: Failable<S, F>
-): value is F {
-  return value instanceof Error;
-}
+): value is F => value instanceof Error;
