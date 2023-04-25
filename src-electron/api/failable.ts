dffc5064d3848032ca5a4636fe16df65b5b7f751
txkodo <kodo.hsmkai@gmail.com>
2023-04-25 21:42:54 +0900

    ✨ failablifyを追加

--- src-electron/api/failable.ts
+++ src-electron/api/failable.ts
@@ -7,3 +7,17 @@ export const isSuccess = <S, F extends Error>(
 export const isFailure = <S, F extends Error>(
   value: Failable<S, F>
 ): value is F => value instanceof Error;
+
+export const failablify =
+  <A extends any[], R>(func: (...args: A) => R) =>
+  (...args: A): Failable<R> => {
+    try {
+      return func(...args);
+    } catch (e) {
+      if (e instanceof Error) {
+        return e;
+      } else {
+        throw e;
+      }
+    }
+  };
