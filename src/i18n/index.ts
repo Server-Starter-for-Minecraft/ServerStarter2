f6eeb6feefd7e7acc78122992ff9b21ebad1d307
txkodo <kodo.hsmkai@gmail.com>
2023-05-04 13:25:20 +0900

    🏷️ i18nにlocaleの型補完が効くように変更

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,15 @@
+import { Locale } from 'app/src-electron/schema/system';
 import enUS from './en-US';
 
-export default {
-  'en-US': enUS
+// 現状enUSのデータ型をスキーマとしている
+type MessageSchema = typeof enUS;
+
+// 言語を追加する場合
+// 1. src-electron/schema/system Locale に言語コードを追加
+// 2. src/i18n/{locale}/{locale}.tsを作成し内容を記述
+// 3. このmassagesに追加
+const massages: Record<Locale, MessageSchema> = {
+  'en-US': enUS,
 };
+
+export default massages;
