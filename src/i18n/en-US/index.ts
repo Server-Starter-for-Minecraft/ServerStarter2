b2a41ef60325c7e5fb4e1739f35fbb77014465e9
CivilTT <civiltt.ss@gmail.com>
2023-05-04 15:48:40 +0900

    ✨ Added the type definision in i18n

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,7 +1,12 @@
 // This is just an example,
 // so you can safely delete all default props below
 
-export default {
+type iTranslate = {
+  failed: string,
+  success: string,
+}
+
+export const enUS: iTranslate = {
   failed: 'Action failed',
   success: 'Action was successful'
 };
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,4 @@
-import enUS from './en-US';
+import { enUS } from './en-US';
 
 export default {
   'en-US': enUS
