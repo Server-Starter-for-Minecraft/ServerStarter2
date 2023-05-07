73a1b20291ce2c56ebefe34db5f8921010af1f51
txkodo <kodo.hsmkai@gmail.com>
2023-05-08 01:38:09 +0900

    ✨ assertCompleteを実装

--- src-electron/lifecycle/lifecycle.ts
+++ src-electron/lifecycle/lifecycle.ts
@@ -2,3 +2,18 @@ import { createAppEvent } from './event';
 
 /** electronのwillQuitイベント(すべてのwindowが閉じた後の処理)で発火されるイベント */
 export const onQuit = createAppEvent();
+
+/**
+ * 非同期処理の途中でアプリケーションが閉じられても、処理の終了を保証する
+ *
+ * 注釈: Windows では、システムのシャットダウン/再起動やユーザーのログアウトでアプリケーションが閉じられようとしている場合には、処理の終了は保証されない
+ */
+export async function assertComplete<T>(promise: Promise<T>) {
+  const dispatch = onQuit(async (dispatch) => {
+    await promise;
+    dispatch();
+  });
+  const result = await promise;
+  dispatch();
+  return result;
+}
