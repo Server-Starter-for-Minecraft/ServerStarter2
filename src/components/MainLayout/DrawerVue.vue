e3fc26f13a680ab26d939e77c5e3b8ffa7db4fa3
CivilTT <civiltt.ss@gmail.com>
2023-05-22 23:51:36 +0900

    🔥 Removed old codes in front

--- src/components/MainLayout/DrawerVue.vue
+++ src/components/MainLayout/DrawerVue.vue
@@ -6,42 +6,12 @@ const store = useMainStore();
 
 <template>  
   <q-drawer
-      v-model="store.leftDrawerOpen"
-      :mini="$q.screen.lt.md"
-      :width="300"
-      :breakpoint="100"
-      bordered
-      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
-    >
-      <q-list>
-        <template v-for="(menuItem, index) in store.drawerContents" :key="index">
-          <q-item
-            clickable
-            v-ripple
-            :active="store.selectedDrawer === menuItem.label"
-            @click="store.selectedDrawer = menuItem.label"
-            :to="menuItem.to"
-            class="q-pa-md"
-          >
-            <q-item-section avatar>
-              <q-icon :name="menuItem.icon" size="1.4rem"/>
-            </q-item-section>
-            <q-item-section>
-              <span style="font-size: 1.2rem;">{{ menuItem.label }}</span>
-            </q-item-section>
-            <q-tooltip
-              v-if="$q.screen.lt.md"
-              anchor="center right"
-              self="center left"
-              :offset="[10, 10]"
-              class="text-body1"
-            >
-              {{ menuItem.label }}
-            </q-tooltip>
-          </q-item>
-          <q-separator :key="'sep' + index"  v-if="menuItem.separator" />
-        </template>
-      </q-list>
-    </q-drawer>
-
+    v-model="store.leftDrawerOpen"
+    :mini="$q.screen.lt.md"
+    :width="300"
+    :breakpoint="100"
+    bordered
+    :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+  >
+  </q-drawer>
 </template>
