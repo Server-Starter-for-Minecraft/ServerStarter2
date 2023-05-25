cd4d654b12e85e24ff7fb7f05df9b5a2c6f4ac54
CivilTT <civiltt.ss@gmail.com>
2023-05-25 23:03:09 +0900

    ✨ Added additional settings in HOME

--- src/pages/WorldTabs/HomePage.vue
+++ src/pages/WorldTabs/HomePage.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { useMainStore } from 'src/stores/MainStore';
 import SsInput from 'src/components/util/base/ssInput.vue';
 import SsSelect from 'src/components/util/base/ssSelect.vue';
+import ExpansionView from 'src/components/World/HOME/expansionView.vue';
 
 const mainStore = useMainStore()
 const world = mainStore.worldList[mainStore.selectedIdx]
@@ -39,21 +40,48 @@ const showSoloWorld = ref(false)
     <!-- TODO: 配布ワールドは新規World以外でも導入できるようにするのか？ -->
     <!-- TODO: 配布ワールドだけでなく、既存の個人ワールドについても.minecraftがある場合は導入できるようにする？ -->
     <!-- 個人ワールドのデフォルトパスは変更できるようにする -->
-    <h1>既存ワールドの導入</h1>
+    <ExpansionView title="既存ワールドの導入">
+      <q-toggle v-model="showSoloWorld" :label="showSoloWorld ? '個人ワールドを導入する' : '配布ワールドをZipファイルより導入する'"/>
+      <p v-show="!showSoloWorld" class="text-caption q-ma-none q-mt-md">インターネットよりダウンロードしたワールド（配布ワールド）を導入する</p>
+      <p v-show="showSoloWorld" class="text-caption q-ma-none q-mt-md">このPC上にあるMinecraftの個人ワールドを導入する</p>
+      <q-file
+        v-show="!showSoloWorld"
+        v-model="customWorldPath"
+        label="配布ワールド(.zip)を選択"
+        clearable
+        accept=".zip"
+      >
+        <template v-slot:prepend>
+          <q-icon name="attach_file" />
+        </template>
+      </q-file>
+      <SsSelect v-show="showSoloWorld" v-model="soloWorldName"/>
+    </ExpansionView>
     <!-- <input type="file" webkitdirectory directory multiple/> -->
-    <q-toggle v-model="showSoloWorld" :label="showSoloWorld ? '個人ワールドを導入する' : '配布ワールドをZipファイルより導入する'"/>
-    <p class="text-caption q-ma-none q-mt-md">インターネットよりダウンロードしたワールド（配布ワールド）を導入する</p>
-    <q-file
-      v-show="!showSoloWorld"
-      v-model="customWorldPath"
-      label="配布ワールド(.zip)を選択"
-      clearable
-      accept=".zip"
-    >
-      <template v-slot:prepend>
-        <q-icon name="attach_file" />
-      </template>
-    </q-file>
-    <SsSelect v-show="showSoloWorld" v-model="soloWorldName"/>
+
+    <!-- TODO: memoryがsizeとunitのpropertyを有していないバグをバックに報告 -->
+    <ExpansionView title="起動設定">
+      <div class="row" style="max-width: 300px;">
+        <SsInput
+          v-model="world.memory"
+          label="メモリサイズ"
+          class="col q-pr-md"
+        />
+        <SsSelect
+          v-model="world.memory"
+          :options="['MB', 'GB', 'TB']"
+          label="単位"
+          class="col-3"
+        />
+      </div>
+
+      <SsInput
+        v-model="world.javaArguments"
+        label="JVM引数"
+        class="q-pt-md"
+      />
+    </ExpansionView>
+
+
   </div>
 </template>
