477a8b3b85b31c558b47229e1a754b737d42e90d
txkodo <kodo.hsmkai@gmail.com>
2023-06-12 00:20:49 +0900

    💥 player周りのスキーマを変更

--- src-electron/schema/brands.ts
+++ src-electron/schema/brands.ts
@@ -2,5 +2,14 @@ import { Brand } from '../util/brand';
 
 export type UUID = Brand<string, 'UUID'>;
 
+/** ワールドコンテナの名前文字列 */
 export type WorldContainer = Brand<string, 'WorldContainer'>;
+
+/** ワールドの名前文字列 */
 export type WorldName = Brand<string, 'WorldName'>;
+
+/** プレイヤーのUUID文字列 */
+export type PlayerUUID = Brand<UUID, 'PlayerUUID'>;
+
+/** 画像のuri文字列 <img src={ここに挿入可能}> */
+export type ImageURI = Brand<string, 'ImageURI'>;
--- src-electron/schema/player.ts
+++ src-electron/schema/player.ts
@@ -1,13 +1,29 @@
+import { ImageURI, PlayerUUID } from './brands';
+
+/** システムのプレイヤー設定 */
 export type Player = {
+  /** プレイヤー名 */
   name: string;
-  uuid: string;
+
+  /** プレイヤーのUUID */
+  uuid: PlayerUUID;
+
+  /** プレイヤースキンの顔部分の画像 */
+  avatar: ImageURI;
+
+  /** プレイヤースキンの顔部分の外側レイヤーの画像 */
+  avatar_overlay: ImageURI;
 };
 
+/** システムのプレイヤーグループ設定 */
 export type PlayerGroup = {
+  /** グループ名 */
   name: string;
-  players: Player[];
+  /** 所属するプレイヤーのUUIDのリスト */
+  players: PlayerUUID[];
 };
 
+/** ワールドごとのプレイヤーOPの権限レベル */
 export type OpLevel = 1 | 2 | 3 | 4;
 
 export type OpSetting = {
@@ -15,11 +31,10 @@ export type OpSetting = {
   bypassesPlayerLimit: boolean;
 };
 
+/** ワールドごとのプレイヤー設定 */
 export type PlayerSetting = {
-  /** プレイヤーの名前 */
-  name: string;
   /** プレイヤーのUUID */
-  uuid: string;
+  uuid: PlayerUUID;
   /** プレイヤーのop権限レベル */
   op?: OpSetting;
 };
