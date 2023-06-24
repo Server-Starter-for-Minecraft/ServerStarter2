dcee56b14557ceb76a9d4fbdb063c47043d00eda
MojaMonchi <linters-triages.0c@icloud.com>
2023-06-24 18:00:44 +0900

    🌐 main画面の翻訳を追加

--- src/i18n/ja/index.ts
+++ src/i18n/ja/index.ts
@@ -1,11 +1,15 @@
 // This is just an example,
 // so you can safely delete all default props below
+import { japroperty } from "src/i18n/ja/property"
 
 export const ja = {
-  failed: 'Action failed',
-  success: 'Action was successful',
-  test: 'これはテスト',
-  property: {
-    levelseed: 'ワールドのシード値',
+  failed: '実行に失敗しました',
+  success: '正常に実行されました',
+  home: {
+    worldname: 'ワールド名',
+    version: 'バージョン',
+    useworld: '既存ワールドの導入',
+    settings: '起動設定',
   },
+  property:japroperty
 };
