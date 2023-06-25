ffe5dc5eb44b966c2e1d035a02994abb0e2f9e71
CivilTT <civiltt.ss@gmail.com>
2023-06-26 00:50:46 +0900

    ✨ Added player cards

--- src/components/World/Player/PlayerCardView.vue
+++ src/components/World/Player/PlayerCardView.vue
@@ -1,15 +1,62 @@
 <script setup lang="ts">
+import { useSystemStore } from 'src/stores/SystemStore';
+import { usePlayerStore } from 'src/stores/WorldTabsStore';
+import { ref } from 'vue';
+
 interface Prop {
-  name: string
-  icon: string
+  uuid: string
   opLevel?: 1 | 2 | 3 | 4
-  groups?: string[]
 }
-defineProps<Prop>()
+const prop = defineProps<Prop>()
+
+const sysStore = useSystemStore()
+const playerStore = usePlayerStore()
+const playerData = sysStore.systemSettings().player.players.filter(p => p.uuid===prop.uuid)[0]
+const focus = ref(false)
+
+function onCardClicked() {
+  // TODO: カードが選択されたら選択一覧にカードを追加し、カードをFocusモードにする
+  // もう一度選択されたら選択一覧から除外し、フォーカスも解除する
+  if (playerStore.focusCards.includes(prop.uuid)) {
+    // playerStore.focusCards.(prop.uuid)
+    focus.value = false
+  }
+  else {
+    focus.value = true
+  }
+}
 </script>
 
 <template>
-  <q-card>
-    
+  <q-card flat bordered :class="`q-ma-xs card ${focus ? 'card-active' : ''}`">
+    <q-item clickable @click="onCardClicked" class="q-pa-md">
+      <q-item-section avatar>
+        <q-avatar square>
+          <q-img :src="playerData.avatar" class="avaterImg"/>
+          <q-img :src="playerData.avatar_overlay" class="avaterImg" style="width: 110%;"/>
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ playerData.name }}</q-item-label>
+        <q-item-label caption>
+          test
+        </q-item-label>
+      </q-item-section>
+    </q-item>
   </q-card>
 </template>
+
+<style scoped lang="scss">
+.card {
+  width: max-content;
+}
+.card-active {
+  border-color: $primary;
+}
+
+.avaterImg {
+  position: absolute;
+  image-rendering: pixelated;
+}
+</style>
--- src/pages/WorldTabs/PlayerPage.vue
+++ src/pages/WorldTabs/PlayerPage.vue
@@ -1,6 +1,56 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useMainStore } from 'src/stores/MainStore';
+import { usePlayerStore } from 'src/stores/WorldTabsStore';
+import SsInput from 'src/components/util/base/ssInput.vue';
+import PlayerCardView from 'src/components/World/Player/PlayerCardView.vue';
+import GroupCardView from 'src/components/World/Player/GroupCardView.vue';
+
+const mainStore = useMainStore()
+const playerStore = usePlayerStore()
+
+const toggle1 = ref(true)
 </script>
 
 <template>
-  <h1>Player Page</h1>
+  <div class="q-px-md">
+    <div class="row">
+      <SsInput
+        v-model="playerStore.searchName"
+        label="プレイヤーを検索"
+        class="q-py-md col"
+        @clear="() => playerStore.searchName = ''"
+      />
+  
+      <q-toggle
+        v-model="toggle1"
+        label="登録済みのプレイヤーのみ参加可能"
+        style="font-size: 1rem;"
+      />
+    </div>
+  
+    <div class="q-py-md fit">
+      <span class="text-caption">追加済みプレイヤー</span>
+      <div class="row items-start">
+        <template v-for="player in playerStore.searchPlayers(mainStore.world().players)" :key="player">
+          <PlayerCardView
+            :uuid="player.uuid"
+            :op-level="player.op?.level"
+          />
+        </template>
+      </div>
+
+      <q-separator class="q-my-md"/>
+
+      <span class="text-caption">グループ一覧</span>
+      <div class="row items-start">
+        <template v-for="group in playerStore.searchGroups()" :key="group">
+          <GroupCardView
+            :name="group.name"
+            :players="group.players"
+          />
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
