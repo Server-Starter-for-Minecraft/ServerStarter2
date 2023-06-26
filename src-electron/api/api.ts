85ff747f00f6854e860654cd31faecf7104ee402
txkodo <kodo.hsmkai@gmail.com>
2023-06-26 23:51:55 +0900

    ✨ APIにWithErrorを使用

--- src-electron/api/api.ts
+++ src-electron/api/api.ts
@@ -6,6 +6,7 @@ import { Version, VersionType } from '../schema/version';
 import { World, WorldAbbr, WorldEdited, WorldID } from '../schema/world';
 import { Failable } from './failable';
 import { IAPI, IBackAPI, IFrontAPI } from './types';
+import { WithError } from './witherror';
 
 /**
  * ## APIの利用方法
@@ -79,40 +80,40 @@ export interface API extends IAPI {
     /** WorldAbbrの一覧を取得 */
     GetWorldAbbrs: (
       worldContainer: WorldContainer
-    ) => Promise<Failable<WorldAbbr[]>>;
+    ) => Promise<WithError<Failable<WorldAbbr[]>>>;
 
     /** Worldの情報を(再)取得 リモートがある場合リモートから(再)取得 */
-    GetWorld: (WorldId: WorldID) => Promise<Failable<World>>;
+    GetWorld: (WorldId: WorldID) => Promise<WithError<Failable<World>>>;
     /** Worldの情報を上書き リモートがある場合リモートの情報も上書き
      * 戻り値の値が実際にセットされた値になる */
-    SetWorld: (world: WorldEdited) => Promise<Failable<World>>;
+    SetWorld: (world: WorldEdited) => Promise<WithError<Failable<World>>>;
     /** 新しいWorldのデータを生成 ディレクトリ等は生成されない */
-    NewWorld: () => Promise<Failable<World>>;
+    NewWorld: () => Promise<WithError<Failable<World>>>;
     /** Worldを生成 実際にディレクトリを生成し、リモートがある場合リモートも生成する */
-    CreateWorld: (world: WorldEdited) => Promise<Failable<World>>;
+    CreateWorld: (world: WorldEdited) => Promise<WithError<Failable<World>>>;
     /** Worldを削除 リモートがある場合でもリモートは削除しない */
-    DeleteWorld: (world: WorldID) => Promise<Failable<void>>;
+    DeleteWorld: (world: WorldID) => Promise<WithError<Failable<void>>>;
 
     /** サーバーを起動 */
-    RunServer: (world: WorldID) => Promise<Failable<World>>;
+    RunServer: (world: WorldID) => Promise<WithError<Failable<World>>>;
 
     /** プレイヤーを名前またはUUIDで検索する(完全一致のみ) */
-    SearchPlayer: (nameOrUuid: string) => Promise<Failable<Player>>;
+    SearchPlayer: (nameOrUuid: string) => Promise<WithError<Failable<Player>>>;
 
     /** Version一覧を取得 useCache===trueのときローカルのキャッシュを使用する(高速) */
     GetVersions: (
       type: VersionType,
       useCache: boolean
-    ) => Promise<Failable<Version[]>>;
+    ) => Promise<WithError<Failable<Version[]>>>;
 
     /** ワールド名が使用可能かどうかを検証する */
     ValidateNewWorldName: (
       worldContainer: WorldContainer,
       worldName: string
-    ) => Promise<Failable<WorldName>>;
+    ) => Promise<WithError<Failable<WorldName>>>;
 
     /** ディレクトリを選択する */
-    PickDirectory: () => Promise<Electron.OpenDialogReturnValue>;
+    PickDirectory: () => Promise<WithError<Electron.OpenDialogReturnValue>>;
   };
 }
 
