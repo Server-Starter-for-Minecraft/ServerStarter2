39e17ec7e36a989a0f1e418d5a582355dec38807
nozz-mat <minaharu318@gmail.com>
2023-06-27 01:10:33 +0900

    🌐 翻訳文を追加

--- src/i18n/en-US/console.ts
+++ src/i18n/en-US/console.ts
@@ -1,3 +1,4 @@
 export const enUSConsole = {
   boot: 'boot server',
+  abnormalEnd:'Terminated abnormally'
 }
--- src/i18n/ja/console.ts
+++ src/i18n/ja/console.ts
@@ -1,3 +1,4 @@
 export const jaConsole = {
   boot: 'サーバーを起動',
+  abnormalEnd:'サーバーが以上終了しました'
 }
