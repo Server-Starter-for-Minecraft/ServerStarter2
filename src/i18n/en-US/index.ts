5aa7ba57f91819954f8d8a94c772331b6b58b35d
MojaMonchi <linters-triages.0c@icloud.com>
2023-06-29 00:35:37 +0900

    🚧 index.tsの翻訳を追加

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,12 +1,19 @@
 // This is just an example,
 // so you can safely delete all default props below
 
+import { ErrorTranslationTypes } from 'app/src-electron/util/error/schema';
+
 type iTranslate = {
-  failed: string,
-  success: string,
-}
+  failed: string;
+  success: string;
+  error: ErrorTranslationTypes;
+};
 
 export const enUS: iTranslate = {
   failed: 'Action failed',
-  success: 'Action was successful'
+  success: 'Action was successful',
+  error: {
+    system: { runtime: 'Error', ipc: 'a', subprocess: 'c' },
+    data: {}
+  },
 };
