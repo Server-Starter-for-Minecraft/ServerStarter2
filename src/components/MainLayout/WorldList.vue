b786685cd06df7e6f09d70eb7fd2edcf43ffa86d
CivilTT <civiltt.ss@gmail.com>
2023-07-12 23:03:01 +0900

    ✨ Added splitter between drawer and main World Pages

--- src/components/MainLayout/WorldList.vue
+++ src/components/MainLayout/WorldList.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { assets } from 'src/assets/assets';
 import { useMainStore } from 'src/stores/MainStore';
 import WorldTab from './WorldTab.vue';
 import SearchWorldView from '../World/SearchWorldView.vue';
 import IconButtonView from '../World/utils/IconButtonView.vue';
-import { assets } from 'src/assets/assets';
 
 const mainStore = useMainStore();
 const searchWorldName = ref('')
-const drawer = ref(true)
-const miniDrawer = ref(true)
 
 function openHP() {
   window.API.sendOpenBrowser('https://civiltt.github.io/ServerStarter/')
@@ -17,37 +15,38 @@ function openHP() {
 </script>
 
 <template>
-  <q-drawer
-    v-model="drawer"
-    :breakpoint="1"
-    :mini="miniDrawer"
-    :mini-width="100"
-    @mouseover="miniDrawer = false"
-    @mouseout="miniDrawer = true"
-    :class="`column ${$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'}`"
-    style="height: 100vh;"
-  >
-    <q-img :src="assets.png.mainImage" class="img">
-      <div class="absolute-full">
-        <q-img :src="assets.svg.systemLogo" fit="contain" style="height: 100%;"/>
-        <q-btn class="absolute-full" @click="openHP"/>
-      </div>
-    </q-img>
+  <q-img :src="assets.png.mainImage" class="img">
+    <div class="absolute-full">
+      <!-- TODO: 画像が読み込まれない問題の修正 -->
+      <q-img
+        :src="assets.svg.systemLogo_text_dark"
+        fit="contain"
+        class="q-mini-drawer-hide"
+        style="height: 100%;"
+      />
+      <q-img
+        :src="assets.svg.systemLogo"
+        fit="contain"
+        class="q-mini-drawer-only"
+        style="height: 100%;"
+      />
+      <q-btn class="absolute-full" @click="openHP"/>
+    </div>
+  </q-img>
 
-    <search-world-view v-model="searchWorldName"/>
-    
-    <q-scroll-area class="fit col">
-      <q-list>
-        <template v-for="(world, idx) in mainStore.searchWorld(searchWorldName)" :key="world">
-          <world-tab :world="world" :idx="idx"/>
-        </template>
-      </q-list>
-    </q-scroll-area>
+  <search-world-view v-model="searchWorldName"/>
+  
+  <q-scroll-area class="fit col">
+    <q-list>
+      <template v-for="(world, idx) in mainStore.searchWorld(searchWorldName)" :key="world">
+        <world-tab :world="world" :idx="idx"/>
+      </template>
+    </q-list>
+  </q-scroll-area>
 
-    <icon-button-view icon-name="add" label="ワールドを追加" @click="mainStore.createNewWorld()"/>
-    <q-separator class="q-mx-xs"/>
-    <icon-button-view icon-name="settings" label="システム設定" to="/system"/>
-  </q-drawer>
+  <icon-button-view icon-name="add" label="ワールドを追加" @click="mainStore.createNewWorld()"/>
+  <q-separator class="q-mx-xs"/>
+  <icon-button-view icon-name="settings" label="システム設定" to="/system"/>
 </template>
 
 <style scoped lang="scss">
