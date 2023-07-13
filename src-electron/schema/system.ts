c4132aaaf5f2b224b489b79a2005701a8499a515
txkodo <kodo.hsmkai@gmail.com>
2023-07-14 00:15:21 +0900

    🍱 themeアイコンを追加

--- src-electron/schema/system.ts
+++ src-electron/schema/system.ts
@@ -22,10 +22,10 @@ export type SystemSettingsEdited = {
   user: SystemUserSetting;
 };
 
-export const locales = ['ja', 'en-US'] as const
+export const locales = ['ja', 'en-US'] as const;
 export type Locale = (typeof locales)[number];
 
-export const colorThemes = ['auto', 'light', 'dark'] as const
+export const colorThemes = ['auto', 'light', 'dark'] as const;
 export type ColorTheme = (typeof colorThemes)[number];
 
 /** ローカルのワールドの保存先ディレクトリ (絶対パスのみ) */
