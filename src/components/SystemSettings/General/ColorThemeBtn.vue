7e5e798fe3fa4b0961120e1d715cb9c8f1166f13
CivilTT <civiltt.ss@gmail.com>
2023-07-15 23:39:34 +0900

    ✨ Changed color theme's UI

---
+++ src/components/SystemSettings/General/ColorThemeBtn.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Prop {
+  src: string
+  active?: boolean
+  label: string
+  onClick: () => void
+}
+defineProps<Prop>()
+</script>
+
+<template>
+  <q-btn
+    dense
+    outline
+    :color="active ? 'primary' : 'transparent'"
+    @click="onClick"
+  >
+    <q-avatar square size="3rem" class="q-ma-xs">
+      <q-img :src="src" />
+    </q-avatar>
+
+    <q-tooltip class="text-body2">
+      {{ label }}
+    </q-tooltip>
+  </q-btn>
+</template>
