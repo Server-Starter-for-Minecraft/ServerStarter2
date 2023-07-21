0cf5d647dd5ec2b77c54be1a075f4d28dd39038a
CivilTT <civiltt.ss@gmail.com>
2023-07-22 00:40:31 +0900

    💡 Added comments in Remote page

--- src/components/World/ShareWorld/NewRemoteView.vue
+++ src/components/World/ShareWorld/NewRemoteView.vue
@@ -2,6 +2,7 @@
 import { useQuasar } from 'quasar';
 import { values } from 'src/scripts/obj';
 import { useSystemStore } from 'src/stores/SystemStore';
+import { useMainStore } from 'src/stores/MainStore';
 import { GithubAccountSetting } from 'app/src-electron/schema/remote';
 import GithubCard from 'src/components/SystemSettings/Remote/github/GithubCard.vue';
 import AddContentsCard from 'src/components/util/AddContentsCard.vue';
@@ -9,6 +10,7 @@ import NewRemoteDialog from 'src/components/SystemSettings/Remote/NewRemoteDialo
 
 const $q = useQuasar()
 const sysStore = useSystemStore()
+const mainStore = useMainStore()
 
 function addRemote() {
   $q.dialog({
@@ -19,6 +21,9 @@ function addRemote() {
 function registerRemote(remoteData: GithubAccountSetting) {
   // TODO: github以外に対応した場合は条件分岐を入れる
   // TODO: remoteDataから生成する場合、branchキーに何を入れるのか？
+  // --> 基本的にはワールド名だが、新規でリモートを登録する際にはbranch名をユーザーが変更できるようなモーダルを挟む
+  // remote_sourceには既存のリモートワールドを導入する際にそのワールドの登録情報を入れる
+  // remoteには新規のリモートと既存のリモートの登録情報を入れる
   console.log('### TODO: 登録処理の実装 ###')
 }
 </script>
