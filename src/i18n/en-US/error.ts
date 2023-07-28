47cd312873a06dcbcbd8e7d6909db6d790b76681
MojaMonchi <linters-triages.0c@icloud.com>
2023-07-29 04:27:19 +0900

    🌐 error.tsの翻訳

--- src/i18n/en-US/error.ts
+++ src/i18n/en-US/error.ts
@@ -1,70 +1,82 @@
 /* バックエンド周りのエラーの翻訳 */
 
-import { ErrorTranslationTypes } from "app/src-electron/schema/error";
+import { ErrorTranslationTypes } from 'app/src-electron/schema/error';
 
 export const enUSError: ErrorTranslationTypes = {
-    system:  {
-        runtime: '',
-        ipc: '',
-        subprocess: ''
+  system: {
+    runtime: '\
+      Runtime error occurred.<br>\
+      Error type: {type}<br>\
+      Error message: {message}',
+    ipc: '\
+      IPC error occuerred<br>\
+      Error type: {type}<br>\
+      Error message: {message}',
+    subprocess: '\
+      Error occurred in {processPath}<br>\
+      Exitcode: {exitcode}',
+  },
+  data: {
+    url: {
+      fetch: 'Failed to get data from {url}',
     },
-    data: {
-        url: {
-            fetch: ''
-        },
-        path: {
-            loadingFailed: '',
-            alreadyExists: '',
-            notFound: '',
-            invalidContent: {
-              missingJavaCommand: '',
-              mustBeDirectory: '',
-              mustBeFile: '',
-              invalidDatapack: '',
-              invalidPlugin: '',
-              invalidMod: '',
-              invalidCustomMap: '',
-              customMapZipWithMultipleLevelDat: '',
-              invalidWorldSettingJson: '',
-              invalidOpsJson: '',
-              invalidWhitelistJson: ''
-            },
-            creationFiled: '',
-            dialogCanceled: '',
-        },
-        hashNotMatch: ''
+    path: {
+      loadingFailed: 'Failed to read the file or directory',
+      alreadyExists: 'File or directory already exists',
+      notFound: 'No file or directory exists',
+      invalidContent: {
+        missingJavaCommand: 'There are no command to exec java in run.bat or run.sh',
+        mustBeDirectory: 'You need to use directory here',
+        mustBeFile: 'You need to select file here',
+        invalidDatapack: 'Improper datapack is selected',
+        invalidPlugin: 'Improper plugin file is selected',
+        invalidMod: 'Improper Mod file is selected',
+        invalidCustomMap: 'Improper custom map is select',
+        customMapZipWithMultipleLevelDat: 'There are too many level.dat in the custom map saved in {path}',
+        invalidWorldSettingJson: 'server_setting.json is not inappropriate',
+        invalidOpsJson: 'ops.json is not inappropriate',
+        invalidWhitelistJson: 'ops.json is not inappropriate',
+      },
+      creationFiled: 'Failed to create file or directory',
+      dialogCanceled: 'Window to select file is cancelled',
     },
-    value: {
-        playerName: '',
-        playerUUID: '',
-        playerNameOrUUID: '',
-        base64URI: '',
-        commandLineArgument: '',
-        worldName: {
-            notMatchRegex: '',
-            alreadyUsed: ''
-        },
+    hashNotMatch: 'The file may not be correct',
+  },
+  value: {
+    playerName: '{value} does not exist',
+    playerUUID: 'No players are related to this ID',
+    playerNameOrUUID: 'Nethier player exist related {value}',
+    base64URI: 'No figure exist in {value}',
+    commandLineArgument: 'Wrong command line, {value} does not exist',
+    worldName: {
+      notMatchRegex: 'Inallowed letter is used, just use an alphabet or number',
+      alreadyUsed: 'This name is already used',
     },
-    core: {
-        world: {
-          invalidWorldId: '',
-          cannotChangeRunningWorldName: '',
-          worldAleradyRunning: '',
-          failedChangingOp: '',
-        },
-        container: {
-          noContainerSubscribed: ''
-        },
-        version: {
-          forgeInstallerNotProvided: '',
-          failSpigotBuild: {
-            javaNeeded: ''
-          },
-          vanillaVersionNotExists: ''
-        },
-        missingPersonalAccessToken: '',
-        minecraftEULANotAccepted: '',
-        failCacheAddiltionalData: '',
-        failGetGlobalIP: ''
+  },
+  core: {
+    world: {
+      invalidWorldId: 'There is no world in {id}',
+      cannotChangeRunningWorldName: '\
+        You cannot change your world name or saved folder<br>\
+        Close your world and do it again',
+      worldAleradyRunning: 'Your world saved in {container}/{name} is already started',
+      failedChangingOp: '\
+        Failed to change op level for {users}<br>\
+        Op level about {users} is {op}',
     },
+    container: {
+      noContainerSubscribed: 'No WorldContainer is registered, check it again',
+    },
+    version: {
+      forgeInstallerNotProvided: 'No forge installer for {version} exists',
+      failSpigotBuild: {
+        javaNeeded: 'Failed to build spiot for {version}, install Java from {minVersion} to {maxversion}',
+      },
+      vanillaVersionNotExists: 'vanilla does not exist in version {version}',
+    },
+    missingPersonalAccessToken: 'Personal access token for https://github.com/{owner}/{repo}',
+    minecraftEULANotAccepted: 'It does not agree to eula for Minecraft',
+    failCacheAddiltionalData: 'Failed to cache {type}',
+    failGetGlobalIP: 'Failed to get global IP address',
+  },
 };
--- src/i18n/ja/progress.ts
+++ src/i18n/ja/progress.ts
@@ -4,34 +4,34 @@ export const jaProgress: ProgressMessageTranslation = {
   server: {
     preparing: '',
     postProcessing: '',
-    getOwner: '',
+    getOwner: 'オーナーのデータを取得中です',
     eula: {
-      title: '',
-      generating: '',
-      loading: '',
-      saving: '',
-      asking: ''
+      title: 'EULAの同意状況を確認しています',
+      generating: 'eula.txt を生成中です',
+      loading: 'eula.txt を読み込み中です',
+      saving: 'eula.txt に書き込み中です',
+      asking: 'eula.txt に同意するかどうかを問い合わせています'
     },
     local: {
-      loading: '',
-      saving: '',
-      savingSettingFiles: '',
-      formatWorldDirectory: '',
-      reloading: '',
-			checkUsing: '',
-			movingSaveData: ''
+      loading: 'ローカルのワールドデータを読み込み中です',
+      saving: 'ローカルにワールドデータを反映しています',
+      savingSettingFiles: 'ワールド設定ファイルを保存中です',
+      formatWorldDirectory: 'ワールドのデータ構造を変更中です',
+      reloading: 'ワールドデータを再読み込みしています',
+			checkUsing: 'ワールドが使用中かどうか確認しています',
+			movingSaveData: 'ワールドのデータを移動中です'
     },
 		remote: {
-			check: '',
-			pull: '',
-			push: ''
+			check: 'リモートのデータがあるかどうか確認しています',
+			pull: 'リモートのデータをダウンロード中です',
+			push: 'リモートにデータをアップロード中です'
 		},
 		java: {
-			memoryArguments: '',
-			userArguments: ''
+			memoryArguments: 'メモリ容量に関する引数を取得しています',
+			userArguments: 'ユーザー設定に関する引数を取得しています'
 		},
 		version: {
-			getLog4jSettingFile: ''
+			getLog4jSettingFile: 'log4Jの設定ファイルをダウンロード中です'
 		},
   },
 };
