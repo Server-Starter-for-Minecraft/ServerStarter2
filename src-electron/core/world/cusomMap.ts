c54a13b43b07d84bb05e7a0b49a3d58285eff3ec
txkodo <kodo.hsmkai@gmail.com>
2023-08-14 00:15:40 +0900

    ➕ nbtifyからprismarine-nbtに移行

--- src-electron/core/world/cusomMap.ts
+++ src-electron/core/world/cusomMap.ts
@@ -101,7 +101,7 @@ export async function loadCustomMap(
   }
 
   if (isError(dat)) return dat;
-  const datContent = await dat.nbt<LevelDat>('gzip');
+  const datContent = await dat.nbt<LevelDat>();
   if (isError(datContent)) return datContent;
 
   const iconURI = await icon?.encodeURI('image/png');
