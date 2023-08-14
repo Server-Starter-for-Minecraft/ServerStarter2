deecb559960c3629c25d839cb61809521f047c49
txkodo <kodo.hsmkai@gmail.com>
2023-08-14 15:08:42 +0900

    🐛マージのミスを修正

--- quasar.config.js
+++ quasar.config.js
@@ -93,10 +93,10 @@ module.exports = configure(function (/* ctx */) {
             // compositionOnly: false,
             runtimeOnly: false,
 
-        //     // you need to set i18n resource including paths !
-        //     include: path.resolve(__dirname, './src/i18n/**'),
-        //   },
-        // ],
+            // you need to set i18n resource including paths !
+            include: path.resolve(__dirname, './src/i18n/**'),
+          },
+        ],
       ],
     },
 
