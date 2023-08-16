4dedc63d23192a65fbdc1d029c8dff1a6c6049b0
nozz-mat <minaharu318@gmail.com>
2023-08-16 14:03:45 +0900

    🌐 EulaDialog.vueの翻訳を追加

---
+++ src/i18n/en-US/eulaDialog.ts
@@ -0,0 +1,9 @@
+export const enUSEulaDialog= {
+  title: 'Agree Minecraft EULA',
+  agree: 'Agree EULA and execute',
+  desc: '\
+    This server is subject to the terms of {0}.{1}\
+    Please read before use.',
+  eula: 'Minecraft EULA',
+  disagree: 'Disagree EULA'
+}
--- src/i18n/ja/general.ts
+++ src/i18n/ja/general.ts
@@ -2,4 +2,6 @@ export const jaGeneral = {
   cancel: 'キャンセル',
   delete: '削除',
   edit: '編集',
+  file: 'ファイル',
+  directory: 'ディレクトリ'
 };
