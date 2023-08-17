7e089a39ac77b3c5887182a4aa1d73ef945d6985
CivilTT <civiltt.ss@gmail.com>
2023-08-17 16:12:32 +0900

    🐛 Fixed progressview's console progress bar

--- src/pages/ProgressPage.vue
+++ src/pages/ProgressPage.vue
@@ -39,7 +39,7 @@ window.API.onProgress((_event, worldID, progress) => {
   <div class="justify-center column items-center fit">
     <div style="width: 30rem;">
       <h1 style="font-weight: bold;">{{ progressStore.getProgress(mainStore.world.id).title }}</h1>
-      <ProgressView :progress="progressStore.getProgress(mainStore.world.id).progress" />
+      <ProgressView :progress="progressStore.getProgress(mainStore.world.id)?.progress" />
     </div>
   </div>
 </template>
