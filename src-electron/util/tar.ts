d9cdc9fe5aa0324657d3e6ceb682991bdc0364d8
txkodo <kodo.hsmkai@gmail.com>
2023-08-18 01:09:14 +0900

    🔥 不要なインポートを削除

--- src-electron/util/tar.ts
+++ src-electron/util/tar.ts
@@ -4,7 +4,7 @@ import { BytesData } from './bytesData';
 import { Path } from './path';
 import { Failable } from '../schema/error';
 import { fromRuntimeError } from './error/error';
-import { failabilify, safeExecAsync } from './error/failable';
+import { safeExecAsync } from './error/failable';
 
 export async function createTar(
   directoryPath: Path,
