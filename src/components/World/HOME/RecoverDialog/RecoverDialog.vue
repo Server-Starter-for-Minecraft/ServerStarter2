1e1db7e0d80a466741e395a9ae0cf51a344d6737
CivilTT <civiltt.ss@gmail.com>
2023-08-20 01:36:25 +0900

    🚧 Adding RecoverDialog

---
+++ src/components/World/HOME/RecoverDialog/RecoverDialog.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useDialogPluginComponent } from 'quasar';
+import BaseDialogCard from 'src/components/util/baseDialog/baseDialogCard.vue';
+
+defineEmits({...useDialogPluginComponent.emitsObject})
+const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginComponent()
+
+// recover api はOnDilaogOKする前にここで実行する
+</script>
+
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <BaseDialogCard
+      title="バックアップから復旧"
+      @close="onDialogCancel"
+    >
+    </BaseDialogCard>
+  </q-dialog>
+</template>
