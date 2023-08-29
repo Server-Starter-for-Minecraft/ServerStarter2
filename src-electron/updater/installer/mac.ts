165c510b60dd0df3b26c47a58c6f1ce60dda623e
txkodo <kodo.hsmkai@gmail.com>
2023-08-30 02:26:32 +0900

    ✨ updateのコンソール文を翻訳

--- src-electron/updater/installer/mac.ts
+++ src-electron/updater/installer/mac.ts
@@ -3,6 +3,8 @@ import { BytesData } from 'app/src-electron/util/bytesData';
 import { isError } from 'app/src-electron/util/error/error';
 import { app } from 'electron';
 import { mainPath } from 'app/src-electron/core/const';
+import { updateMessage } from './message';
+import { getSystemSettings } from 'app/src-electron/core/stores/system';
 
 /** macの最新版をダウンロードしてインストールして再起動 */
 export const installMac = async (pkgurl: string): Promise<void> => {
@@ -12,15 +14,18 @@ export const installMac = async (pkgurl: string): Promise<void> => {
   if (isError(data)) return;
   await data.write(dest.str(), true);
 
+  const sys = await getSystemSettings();
+
   const sh = mainPath.child('updater.sh');
   const script = await BytesData.fromText(`#!/bin/sh
-echo "updating ServerStarter2..."
+echo "${updateMessage[sys.user.language].main}"
+echo "${updateMessage[sys.user.language].mac_pass}"
 sudo installer -pkg ${dest.absolute().strQuoted()} -target /
 open -a "${app.getPath('exe')}"
 exit 0
 `);
-  if (isError(script)) return
-  await script.write(sh.str(),true)
+  if (isError(script)) return;
+  await script.write(sh.str(), true);
 
   const sub = spawn('open', ['-a', 'Terminal', 'updater.sh'], {
     cwd: mainPath.str(),
--- src-electron/updater/installer/windows.ts
+++ src-electron/updater/installer/windows.ts
@@ -3,6 +3,8 @@ import { BytesData } from 'app/src-electron/util/bytesData';
 import { isError } from 'app/src-electron/util/error/error';
 import { app } from 'electron';
 import { mainPath } from 'app/src-electron/core/const';
+import { updateMessage } from './message';
+import { getSystemSettings } from 'app/src-electron/core/stores/system';
 
 /** windowsの最新版をダウンロードしてインストールして再起動 */
 export const installWindows = async (msiurl: string): Promise<void> => {
@@ -12,9 +14,11 @@ export const installWindows = async (msiurl: string): Promise<void> => {
   if (isError(data)) return;
   await data.write(dest.str(), true);
 
+  const sys = await getSystemSettings();
+
   const bat = mainPath.child('updater.bat');
   await bat.writeText(`@echo off
-echo updating
+echo ${updateMessage[sys.user.language].main}
 msiexec /i updater.msi /qb
 start "" "${app.getPath('exe')}"
 exit`);
