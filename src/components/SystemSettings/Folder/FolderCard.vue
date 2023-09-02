46c4781d297a2f679b632fc9004521e46879cdf1
CivilTT <civiltt.ss@gmail.com>
2023-09-02 13:07:03 +0900

    💄 Fixed WorldFolderCard's style

--- src/components/SystemSettings/Folder/FolderCard.vue
+++ src/components/SystemSettings/Folder/FolderCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
 import { getCssVar, useQuasar } from 'quasar';
 import { WorldContainerSetting } from 'app/src-electron/schema/system';
 import { dangerDialogProp } from 'src/components/util/danger/iDangerDialog';
@@ -7,7 +8,6 @@ import { AddFolderDialogProps, AddFolderDialogReturns } from './iAddFolder';
 import SsBtn from 'src/components/util/base/ssBtn.vue';
 import AddFolderDialog from 'src/components/SystemSettings/Folder/AddFolderDialog.vue';
 import DangerDialog from 'src/components/util/danger/DangerDialog.vue';
-import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n()
 
@@ -26,6 +26,13 @@ const sysStore = useSystemStore()
 
 function switchVisible() {
   return folder.value.visible = !folder.value.visible
+}
+
+function getCardSytleClass(active?: boolean, loading?: boolean) {
+  const returnClasses = []
+  if (active) returnClasses.push('text-primary')
+  if (loading) returnClasses.push('disable')
+  return returnClasses.join(' ')
 }
 
 function editFolder() {
@@ -59,61 +66,76 @@ function removeFolder() {
 <template>
   <q-card
     flat
-    bordered 
+    bordered
+    class="card fit"
     :style="{ 'border-color': active ? getCssVar('primary') : 'transparent' }"
   >
-    <q-item
-      :clickable="onClick !== void 0"
-      :active="active"
-      :disable="loading || disable"
-      @click="onClick"
-    >
-      <q-item-section>
+    <div class="row">
+      <div
+        class="col q-pl-md q-py-md"
+        :class="getCardSytleClass(active, loading)"
+        style="margin: auto 0;"
+      >
         <div class="text-omit" style="font-size: 1.1rem;">{{ folder.name }}</div>
         <div class="text-caption text-omit" style="opacity: .6;">{{ folder.container }}</div>
-      </q-item-section>
-  
-      <q-item-section side>
-        <div class="row q-gutter-sm">
-          <ss-btn
-            dense
-            free-width
-            :icon="folder.visible ? 'visibility' : 'visibility_off'"
-            :disable="sysStore.systemSettings.container.filter(c => c.visible).length === 1 && folder.visible || disable"
-            @click="switchVisible"
-          >
-            <q-tooltip>
-              {{ folder.visible ? $t('systemsetting.folder.tooltipVisible') : $t('systemsetting.folder.tooltipInvisible') }}
-            </q-tooltip>
-          </ss-btn>
-          <ss-btn
-            v-show="showOperationBtns && folder.name !== 'default'"
-            free-width
-            :label="$t('general.edit')"
-            :disable="disable"
-            @click="editFolder"
-          />
-          <ss-btn
-            v-show="showOperationBtns"
-            free-width
-            :label="$t('systemsetting.folder.unregist')"
-            color="red"
-            :disable="sysStore.systemSettings.container.length === 1 || disable"
-            @click="removeFolder"
-          />
-        </div>
-      </q-item-section>
+      </div>
 
-      <div class="absolute-center">
-        <q-circular-progress
-          v-show="loading"
-          indeterminate
-          rounded
-          size="40px"
-          color="primary"
-          class="q-ma-md"
+      <!-- cardをクリックできるようにする -->
+      <div v-if="onClick !== void 0" class="absolute-top fit">
+        <q-btn flat color="transparent" @click="onClick" class="fit"/>
+      </div>
+
+      <div class="row q-gutter-x-sm q-pr-md" style="margin: auto 0;">
+        <ss-btn
+          dense
+          free-width
+          :icon="folder.visible ? 'visibility' : 'visibility_off'"
+          :disable="sysStore.systemSettings.container.filter(c => c.visible).length === 1 && folder.visible || disable"
+          @click="switchVisible"
+        >
+          <q-tooltip>
+            {{ folder.visible ? $t('systemsetting.folder.tooltipVisible') : $t('systemsetting.folder.tooltipInvisible') }}
+          </q-tooltip>
+        </ss-btn>
+        <ss-btn
+          v-show="showOperationBtns && folder.name !== 'default'"
+          free-width
+          :label="$t('general.edit')"
+          :disable="disable"
+          @click="editFolder"
+        />
+        <ss-btn
+          v-show="showOperationBtns"
+          free-width
+          :label="$t('systemsetting.folder.unregist')"
+          color="red"
+          :disable="sysStore.systemSettings.container.length === 1 || disable"
+          @click="removeFolder"
         />
       </div>
-    </q-item>
+    </div>
+
+    <div class="absolute-center">
+      <q-circular-progress
+        v-show="loading"
+        indeterminate
+        rounded
+        size="40px"
+        color="primary"
+        class="q-ma-md"
+      />
+    </div>
   </q-card>
 </template>
+
+<style scoped lang="scss">
+.card {
+  border-radius: 5px;
+}
+
+.disable {
+  opacity: .6;
+  outline: 0;
+  cursor: not-allowed;
+}
+</style>
