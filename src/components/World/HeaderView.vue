c0f23ad5c8fba172537df6c8e76a7322bdb96e0c
CivilTT <civiltt.ss@gmail.com>
2023-10-11 01:09:16 +0900

    ✨ Added ssTooltip

--- src/components/World/HeaderView.vue
+++ src/components/World/HeaderView.vue
@@ -2,6 +2,7 @@
 import { useConsoleStore } from 'src/stores/ConsoleStore';
 import { useMainStore } from 'src/stores/MainStore';
 import { useSystemStore } from 'src/stores/SystemStore';
+import SsTooltip from '../util/base/ssTooltip.vue';
 
 const sysStore = useSystemStore()
 const mainStore = useMainStore()
@@ -17,7 +18,10 @@ const statusColor = {
 <template>
   <div class="flex items-center full-width q-py-sm q-px-md">
     <template v-if="$router.currentRoute.value.path.slice(0, 7) !== '/system'">
-      <div class="title text-omit q-pr-md">{{ mainStore.world.name }}</div>
+      <div class="title text-omit q-pr-md">
+        {{ mainStore.world.name }}
+        <SsTooltip :name="mainStore.world.name" anchor="bottom start" self="center start" />
+      </div>
       <div 
         :class="`text-${statusColor[consoleStore.status(mainStore.world.id)]}`"
         class="q-mr-md"
