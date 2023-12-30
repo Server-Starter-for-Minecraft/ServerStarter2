40ccbb8dac254577f6e29579dd9fa2a04f9eface
CivilTT <civiltt.ss@gmail.com>
2023-12-30 17:51:31 +0900

    🍱 Added capture imgs about Ngrok

---
+++ src/components/World/HOME/Others/Ngrok/steps/slides/ImgDialog.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useDialogPluginComponent } from 'quasar';
+import { ImgDialogProp } from '../iNgrok';
+import BaseDialogCard from 'src/components/util/baseDialog/baseDialogCard.vue';
+
+defineEmits({...useDialogPluginComponent.emitsObject})
+const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent()
+defineProps<ImgDialogProp>()
+</script>
+
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <BaseDialogCard
+     title="画像の詳細"
+     @close="onDialogOK"
+     :style="{'width': `${imgWidth}rem`}"
+    >
+      <q-img :src="img" />
+    </BaseDialogCard>
+  </q-dialog>
+</template>
