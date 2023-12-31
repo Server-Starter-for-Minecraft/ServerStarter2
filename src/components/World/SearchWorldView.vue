6ae0581d3bfdfe9dd704f4cb9a8c4cf5c08aeae2
CivilTT <civiltt.ss@gmail.com>
2023-12-31 22:32:56 +0900

    🩹 Fixed expand width when clicking search btn

--- src/components/World/SearchWorldView.vue
+++ src/components/World/SearchWorldView.vue
@@ -3,6 +3,7 @@ import { useSystemStore } from 'src/stores/SystemStore';
 import SsInput from '../util/base/ssInput.vue';
 
 interface Prop {
+  expandWidth: number
   expandDrawerBtnClickable: boolean
 }
 const model = defineModel<string | number>()
@@ -12,22 +13,14 @@ const sysStore = useSystemStore()
 </script>
 
 <template>
-  <q-item
-    :clickable="expandDrawerBtnClickable"
-    @click="sysStore.systemSettings.user.drawerWidth = 300"
-  >
+  <q-item :clickable="expandDrawerBtnClickable" @click="sysStore.systemSettings.user.drawerWidth = expandWidth">
     <q-item-section avatar>
-      <q-icon name="search" size="2rem" class="q-py-sm"/>
+      <q-icon name="search" size="2rem" class="q-py-sm" />
     </q-item-section>
     <q-item-section>
-      <ss-input v-model="model" :label="$t('mainLayout.searchWorld')"/>
+      <ss-input v-model="model" :label="$t('mainLayout.searchWorld')" />
     </q-item-section>
-    <q-tooltip
-      v-if="expandDrawerBtnClickable"
-      anchor="center middle"
-      self="top middle"
-      :delay="500"
-    >
+    <q-tooltip v-if="expandDrawerBtnClickable" anchor="center middle" self="top middle" :delay="500">
       {{ $t('mainLayout.searchWorld') }}
     </q-tooltip>
   </q-item>
