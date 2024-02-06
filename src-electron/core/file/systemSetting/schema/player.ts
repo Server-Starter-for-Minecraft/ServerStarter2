467766e37899d17f0aa8ed07b30f21572b9c0005
txkodo <kodo.hsmkai@gmail.com>
2024-02-07 07:35:59 +0900

    ✨ PlayerSettingsのfixerを作成

--- src-electron/core/file/systemSetting/schema/player.ts
+++ src-electron/core/file/systemSetting/schema/player.ts
@@ -1,11 +1,7 @@
 import { fixArray, ArrayFixMode } from '../../base/fixer/array';
 import { fixObject } from '../../base/fixer/object';
 import { fixString } from '../../base/fixer/primitive';
-
-export type PlayerSettings$1 = {
-  groups: { [name: string]: PlayerGroup$1 };
-  players: string[];
-};
+import { RecordFixMode, fixRecord } from '../../base/fixer/record';
 
 /** システムのプレイヤーグループ設定 */
 export type PlayerGroup$1 = {
@@ -20,5 +16,15 @@ export type PlayerGroup$1 = {
 export const PlayerGroup$1 = fixObject<PlayerGroup$1>({
   name: fixString,
   color: fixString,
+  players: fixArray(fixString, ArrayFixMode.Skip),
+});
+
+export type PlayerSettings$1 = {
+  groups: { [name: string]: PlayerGroup$1 };
+  players: string[];
+};
+
+export const PlayerSettings$1 = fixObject<PlayerSettings$1>({
+  groups: fixRecord(PlayerGroup$1, RecordFixMode.Skip),
   players: fixArray(fixString, ArrayFixMode.Skip),
 });
