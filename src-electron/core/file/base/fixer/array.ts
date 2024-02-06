620d835b30a8e09232c28bac55ae823536461e3e
txkodo <kodo.hsmkai@gmail.com>
2024-02-07 07:22:05 +0900

    ✨ ArrayFixerを追加

---
+++ src-electron/core/file/base/fixer/array.ts
@@ -0,0 +1,39 @@
+import { Fixer, fail, isFail } from './fixer';
+
+export enum ArrayFixMode {
+  /** ItemのFixが失敗した場合、そのItemをスキップ */
+  Skip,
+  /** ItemのFixが失敗した場合、Array自体のFixをFailに */
+  Throw,
+}
+
+export function Array<T>(
+  fixer: Fixer<T, false>,
+  mode: ArrayFixMode
+): Fixer<T[], true>;
+export function Array<T>(
+  fixer: Fixer<T, true>,
+  mode: ArrayFixMode
+): Fixer<T[], true>;
+export function Array<T>(
+  fixer: Fixer<T, boolean>,
+  mode: ArrayFixMode
+): Fixer<T[], true> {
+  const func = (value: any, path: string) => {
+    if (!(value instanceof global.Array)) {
+      return fail([path]);
+    }
+    const fixed: T[] = [];
+    for (const item of value) {
+      const fixedItem = fixer.fix(item, path);
+      if (isFail(fixedItem)) {
+        if (mode === ArrayFixMode.Skip) continue;
+        return fixedItem;
+      }
+      fixed.push(fixedItem);
+    }
+    return fixed;
+  };
+
+  return new Fixer(func);
+}
