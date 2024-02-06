522d4765ee140873f50045316ab038a4e08a1546
txkodo <kodo.hsmkai@gmail.com>
2024-02-07 06:30:20 +0900

    🐛 UnionFixerの型を修正

--- src-electron/core/file/base/fixer/union.ts
+++ src-electron/core/file/base/fixer/union.ts
@@ -1,20 +1,21 @@
 import { Fixer, fail, isFail } from './fixer';
+import { Boolean, Number, String } from './primitive';
 
 export function Union<T extends any[]>(
   ...fixers: {
     [K in keyof T]: Fixer<T[K], false>;
   }
-): Fixer<T, false>;
+): Fixer<T[number], false>;
 export function Union<T extends any[]>(
   ...fixers: {
     [K in keyof T]: Fixer<T[K], true>;
   }
-): Fixer<T, true>;
+): Fixer<T[number], true>;
 export function Union<T extends any[]>(
   ...fixers: {
     [K in keyof T]: Fixer<T[K], boolean>;
   }
-): Fixer<T, boolean> {
+): Fixer<T[number], boolean> {
   const func = (value: any, path: string) => {
     const fails: string[] = [];
     for (const fixer of fixers) {
