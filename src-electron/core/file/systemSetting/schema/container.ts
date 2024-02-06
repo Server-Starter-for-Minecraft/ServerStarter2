0cd6ae681ffbc02e0f433e4068166ca55cd846a4
txkodo <kodo.hsmkai@gmail.com>
2024-02-07 07:22:49 +0900

    ✨ ContainersSettingsにFixerを設定

--- src-electron/core/file/systemSetting/schema/container.ts
+++ src-electron/core/file/systemSetting/schema/container.ts
@@ -1,7 +1,22 @@
-export type ContainersSettings$1 = Container$1[];
+import { Array, ArrayFixMode } from '../../base/fixer/array';
+import { Object } from '../../base/fixer/object';
+import { Boolean, String } from '../../base/fixer/primitive';
 
 export type Container$1 = {
   container: string;
   visible: boolean;
   name: string;
 };
+
+export const Container$1 = Object<Container$1>({
+  container: String,
+  visible: Boolean.default(true),
+  name: String,
+});
+
+export type ContainersSettings$1 = Container$1[];
+
+export const ContainersSettings$1 = Array<Container$1>(
+  Container$1,
+  ArrayFixMode.Skip
+).default([]);
