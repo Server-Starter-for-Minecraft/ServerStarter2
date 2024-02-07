b088c9207e38ed05a6740761d480f3a48e751814
txkodo <kodo.hsmkai@gmail.com>
2024-02-08 02:55:28 +0900

    🎨 fixMapの関数の戻り値にFailを許容

--- src-electron/core/file/base/fixer/map.ts
+++ src-electron/core/file/base/fixer/map.ts
@@ -1,9 +1,18 @@
 import { Fail, Fixer, isFail } from './fixer';
 
+/** fixに成功した場合にその内容を変換する */
 export function fixMap<T, U, F extends boolean>(
   fixer: Fixer<T, F>,
   func: (value: T) => U
-): Fixer<U, F> {
+): Fixer<U, F>;
+export function fixMap<T, U, F extends boolean>(
+  fixer: Fixer<T, F>,
+  func: (value: T) => U | Fail
+): Fixer<U, true>;
+export function fixMap<T, U, F extends boolean>(
+  fixer: Fixer<T, F>,
+  func: (value: T) => U | Fail
+): Fixer<U, boolean> {
   const f = (value: any, path: string) => {
     const fixed: T | Fail = fixer.fix(value, path);
     return isFail(fixed) ? fixed : func(fixed);
