286bb8de633d8cd3e4037ea1dd24ba243ee52b46
txkodo <kodo.hsmkai@gmail.com>
2024-02-07 23:03:44 +0900

    🐛 WorldContainerがひとつも存在しない場合にデフォルト値を設定

--- src-electron/core/file/systemSetting/schema/container.ts
+++ src-electron/core/file/systemSetting/schema/container.ts
@@ -16,7 +16,18 @@ export const Container$1 = fixObject<Container$1>({
 
 export type ContainersSettings$1 = Container$1[];
 
+export const defaultContainersSettings$1: ContainersSettings$1 = [
+  {
+    container: './servers',
+    name: 'servers',
+    visible: true,
+  },
+];
+
 export const ContainersSettings$1 = fixArray<Container$1>(
   Container$1,
   ArrayFixMode.Skip
-).default([]);
+)
+  // 一つもコンテナが存在しない場合は defaultContainersSettings$1 を設定する
+  .map((x) => (x.length === 0 ? defaultContainersSettings$1 : x))
+  .default(defaultContainersSettings$1);
