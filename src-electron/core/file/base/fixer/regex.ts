976691306e4909e329d02f8123f4169d2624e697
txkodo <kodo.hsmkai@gmail.com>
2024-02-09 03:41:08 +0900

    ✨ BrandedTypeのfixerを作成

--- src-electron/core/file/base/fixer/regex.ts
+++ src-electron/core/file/base/fixer/regex.ts
@@ -5,11 +5,3 @@ import { fixString } from './primitive';
 export function fixRegex(regex: string | RegExp) {
   fixString.map((x, p) => (x.match(regex) ? x : fail([p])));
 }
-
-const UUIDRegex =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
-
-export const fixUUID = fixString.map((x, p) => {
-  const upper = x.toUpperCase();
-  return upper.match(UUIDRegex) ? upper : fail([p]);
-});
