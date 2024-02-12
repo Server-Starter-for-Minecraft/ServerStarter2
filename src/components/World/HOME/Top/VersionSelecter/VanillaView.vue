790f24fa92c895f33d0bace6551053150c89e46f
CivilTT <civiltt.ss@gmail.com>
2024-02-12 18:56:32 +0900

    ✨ Added sample warning dialog for vanilla versions

--- src/components/World/HOME/Top/VersionSelecter/VanillaView.vue
+++ src/components/World/HOME/Top/VersionSelecter/VanillaView.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import { AllVanillaVersion } from 'app/src-electron/schema/version';
+import { useQuasar } from 'quasar';
+import { AllVanillaVersion, VanillaVersion } from 'app/src-electron/schema/version';
 import { useMainStore } from 'src/stores/MainStore';
 import { useConsoleStore } from 'src/stores/ConsoleStore';
+import { dangerDialogProp } from 'src/components/util/danger/iDangerDialog';
 import SsSelect from 'src/components/util/base/ssSelect.vue';
+import DangerDialog from 'src/components/util/danger/DangerDialog.vue';
 
 interface Prop {
   versionData: AllVanillaVersion;
 }
 const prop = defineProps<Prop>();
 
+const $q = useQuasar()
 const mainStore = useMainStore();
 const consoleStore = useConsoleStore();
 
@@ -26,12 +30,52 @@ if (mainStore.world.version.type !== 'vanilla') {
   mainStore.world.version =
     vanillaOps().find((ops) => ops.release) ?? vanillaOps()[0];
 }
+let currentVersion = mainStore.world.version
+
+function openWarningDialog(newVer: VanillaVersion) {
+  // Dialog内容の確認用
+  // Version比較が実装された段階で，下記のコードに切り替える
+  $q.dialog({
+    component: DangerDialog,
+    componentProps: {
+      dialogTitle: 'バージョンダウンの確認',
+      dialogDesc: 'サーバーのバージョンを下げる操作は，ワールドデータが破損する恐れがあります．<br>危険性を理解した上でバージョンの変更を行いますか？',
+      okBtnTxt: '危険性を理解して変更する',
+    } as dangerDialogProp,
+  }).onOk(() => {
+    console.log('Accept version down')
+    currentVersion = newVer
+  }).onCancel(() => {
+    console.log('Denied version down')
+    mainStore.world.version = currentVersion
+  })
+
+  // 【切り替え用コード】
+  // if (/** newVer < oldVer */) {
+  //   $q.dialog({
+  //     component: DangerDialog,
+  //     componentProps: {
+  //       dialogTitle: 'バージョンダウンの確認',
+  //       dialogDesc: 'サーバーのバージョンを下げる操作は，ワールドデータが破損する恐れがあります．<br>危険性を理解した上でバージョンダウンを行いますか？',
+  //       okBtnTxt: '危険性を理解してバージョンダウンを行う',
+  //     } as dangerDialogProp,
+  //   }).onOk(() => {
+  //     currentVersion = newVer
+  //   }).onCancel(() => {
+  //     mainStore.world.version = currentVersion
+  //   });
+  // }
+  // else {
+  //   currentVersion = newVer
+  // }
+}
 </script>
 
 <template>
   <div class="row justify-between q-gutter-md items-center">
     <SsSelect
       v-model="mainStore.world.version"
+      @update:model-value="newVal => openWarningDialog(newVal)"
       :options="
         vanillaOps()
           ?.filter((ver, idx) => !isRelease || idx == 0 || ver['release'])
