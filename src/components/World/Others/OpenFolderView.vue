e4378db0c17450c5d37c32c8b1bbe74cec6b7698
CivilTT <civiltt.ss@gmail.com>
2024-02-17 23:06:30 +0900

    ✨ Added open folder btn

---
+++ src/components/World/Others/OpenFolderView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import SsBtn from 'src/components/util/base/ssBtn.vue';
+import { useMainStore } from 'src/stores/MainStore';
+
+const mainStore = useMainStore()
+
+function openFolder() {
+
+}
+</script>
+
+<template>
+  <div class="q-pl-md">
+    <div class="text-caption" style="opacity: 0.6">
+      ワールドデータの保存場所となっているフォルダを開く
+    </div>
+    <SsBtn
+      label="フォルダを開く"
+      @click="openFolder"
+      class="q-mt-md"
+    />
+  </div>
+</template>
--- src/pages/WorldTabs/OthersPage.vue
+++ src/pages/WorldTabs/OthersPage.vue
@@ -2,13 +2,17 @@
 import BootSettingsView from 'src/components/World/Others/BootSettingsView.vue';
 import BackupWorldView from 'src/components/World/Others/BackupWorldView.vue';
 import WorldFolderView from 'src/components/World/Others/WorldFolderView.vue';
+import OpenFolderView from 'src/components/World/Others/OpenFolderView.vue';
 </script>
 
 <template>
   <q-scroll-area ref="scrollAreaRef" class="full-height" style="flex: 1 1 0">
     <div class="mainField">
+      <h1 class="q-pt-lg">保存場所を開く</h1>
+      <OpenFolderView />
+
       <!-- Others -->
-      <h1 class="q-pt-lg">{{ $t('home.setting.title') }}</h1>
+      <h1>{{ $t('home.setting.title') }}</h1>
       <BootSettingsView />
 
       <h1>{{ $t('others.backup.title') }}</h1>
