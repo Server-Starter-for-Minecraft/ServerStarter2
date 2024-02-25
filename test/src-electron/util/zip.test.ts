c6421e7c7074355159e3dd3e6c59687da8919b21
txkodo <kodo.hsmkai@gmail.com>
2024-02-25 16:23:49 +0900

    ✅ util.zipのテストを追加

---
+++ test/src-electron/util/zip.test.ts
@@ -0,0 +1,19 @@
+import { zip } from 'app/src-electron/util/zip';
+
+describe('example', async () => {
+  test('example test 01', () => {
+    const a = [1, 2, 3, 4, 5];
+    const b = ['a', 'b', 'c', 'd'];
+
+    const result = [
+      [1, 'a'],
+      [2, 'b'],
+      [3, 'c'],
+      [4, 'd'],
+    ];
+
+    expect(zip(a, b)).toEqual(result);
+
+    expect(zip([], [])).toEqual([]);
+  });
+});
