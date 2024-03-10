e7890bd15bb8d61bfb80a806f7bb56abb7b1348d
txkodo <kodo.hsmkai@gmail.com>
2024-03-10 15:44:44 +0900

    notifyUpdateでonReadyWindowを使用

--- src-electron/updater/notify.ts
+++ src-electron/updater/notify.ts
@@ -1,10 +1,11 @@
 import { api } from 'app/src-electron/core/api';
 import { OsPlatform } from 'app/src-electron/util/os';
+import { onReadyWindow } from '../lifecycle/lifecycle';
 
-/** linuxの最新版があることを通知 */
+/** linuxの最新版があることをwindowが生成されてから通知 */
 export const notifyUpdate = async (
   type: OsPlatform,
   systemVersion: string
 ): Promise<void> => {
-  await api.send.NotifySystemUpdate(type, systemVersion);
+  onReadyWindow(() => api.send.NotifySystemUpdate(type, systemVersion), true);
 };
