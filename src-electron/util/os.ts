ae5aac349353bc221aad98cb13be2f632ff8d3f1
txkodo <kodo.hsmkai@gmail.com>
2024-04-21 22:05:31 +0900

    🐛 getLinuxTypeの実装を変更

--- src-electron/util/os.ts
+++ src-electron/util/os.ts
@@ -37,18 +37,46 @@ function getOsPlatform(): OsPlatform {
 import { execSync } from 'child_process';
 
 function getLinuxType() {
-  // Execute the command to check the distribution
-  const command = 'cat /etc/os-release | grep "ID_LIKE"';
+  // ID_LIKEをとってくる
+  try {
+    const idLikeCommand = 'cat /etc/os-release | grep "^ID_LIKE="';
+    const outstr = execSync(idLikeCommand, { encoding: 'utf-8' });
+    // Parse the output to determine the Linux type
+    if (outstr.includes('debian')) {
+      return 'debian';
+    } else if (outstr.includes('rhel')) {
+      return 'redhat';
+    } else {
+      return undefined;
+    }
+  } catch {
+    // ID_LIKEがとれなかったらIDをとってくる
+    // OSごとに取ってるので追加する可能性あり
 
-  const outstr = execSync(command, { encoding: 'utf-8' });
+    // Execute the command to check the distribution
+    const command = 'cat /etc/os-release | grep "^ID="';
 
-  // Parse the output to determine the Linux type
-  if (outstr.includes('debian')) {
-    return 'debian';
-  } else if (outstr.includes('rhel')) {
-    return 'redhat';
-  } else {
-    return undefined;
+    try {
+      const outstr = execSync(command, { encoding: 'utf-8' });
+      const idMatch = outstr.match(/(?:^|\n)ID\s*=(.*)\s*/);
+      if (idMatch === null) return idMatch;
+      const id = idMatch[1];
+
+      switch (id) {
+        case 'rhel': // Red Hat Enterprise Linux
+        case 'centos': // CentOS/CentOS Stream
+        case 'ol': // Oracle Linux
+        case 'amzn': // Amazon Linux
+        case 'almalinux': // Almalinux
+          return 'redhat';
+        case 'ubuntu': // Ubuntu
+          return 'debian';
+        default:
+          return undefined;
+      }
+    } catch {
+      return undefined;
+    }
   }
 }
 
