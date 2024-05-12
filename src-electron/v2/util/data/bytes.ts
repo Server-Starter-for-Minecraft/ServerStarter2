23904068ef4fa99e77e2ad8a4dc673789f1969a4
txkodo <kodo.hsmkai@gmail.com>
2024-05-13 01:02:29 +0900

    ✨ bytesを定義

---
+++ src-electron/v2/util/data/bytes.ts
@@ -0,0 +1,61 @@
+import * as stream from 'stream';
+import { Err, Result, err, ok } from '../base';
+import { Readable, ReadableStreamer, WriteStreamer } from './stream';
+
+export class Bytes implements ReadableStreamer {
+  static write(readable: stream.Readable): Promise<Result<Bytes, Error>> {
+    const buffers: Buffer[] = [];
+    let e: undefined | Err<Error> = undefined;
+
+    readable.on('error', (error) => (e = err(error)));
+
+    readable.on('data', (chunk) => buffers.push(chunk));
+
+    return new Promise<Result<Bytes, Error>>((resolve) => {
+      readable.on('close', () => {
+        if (e !== undefined) return resolve(e);
+        resolve(ok(new Bytes(Buffer.concat(buffers))));
+      });
+    });
+  }
+
+  readonly data: Buffer;
+
+  constructor(data: Buffer) {
+    this.data = data;
+  }
+
+  createReadStream(): Readable {
+    const readable = new stream.Readable();
+    const len = this.data.byteLength;
+    let i = 0;
+
+    readable._read = (n) => {
+      if (len <= i) return readable.push(null);
+      readable.push(this.data.subarray(i, i + n));
+      i += n;
+    };
+
+    return new Readable(readable);
+  }
+
+  convert(duplex: stream.Duplex): Readable {
+    return this.createReadStream().convert(duplex);
+  }
+
+  to<T>(target: WriteStreamer<T>): Promise<Result<T, Error>> {
+    return this.createReadStream().to(target);
+  }
+}
+
+/** In Source Testing */
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest;
+  test('bytes', async () => {
+    const bytes = new Bytes(Buffer.from('hello world / こんにちは世界'));
+    const copy = await bytes.to(Bytes);
+    expect(copy.value.data.toString('utf8')).toBe(
+      'hello world / こんにちは世界'
+    );
+  });
+}
