a634a8dd6c3f7a068f6b44fa42034e1a746b2154
CivilTT <civiltt.ss@gmail.com>
2024-05-14 00:36:38 +0900

    ✨ Supported hash streamer for .into()

--- src-electron/v2/util/binary/hash.ts
+++ src-electron/v2/util/binary/hash.ts
@@ -1,7 +1,59 @@
+import { Err, Result, err, ok } from '../base';
 import { IWritableStreamer } from './stream';
+import { createHash } from 'crypto';
 
-const SHA256: IWritableStreamer<string> = {
-  write(readable) {
-    readable;
-  },
-};
+type HashAlgorithm = 'sha256' | 'sha1' | 'md5';
+function hashFunc(algorithm: HashAlgorithm): IWritableStreamer<string> {
+  return {
+    write(readable) {
+      let e: undefined | Err<Error> = undefined;
+
+      const hash = createHash(algorithm);
+      readable.on('error', hash.destroy);
+      const returnHash = readable
+        .pipe(hash)
+        .on('error', (error) => (e = err(error)));
+
+      return new Promise<Result<string, Error>>((resolve) => {
+        readable.on('close', () => {
+          if (e !== undefined) return resolve(e);
+          resolve(ok(returnHash.digest().toString('hex')));
+        });
+      });
+    },
+  };
+}
+
+export const SHA256: IWritableStreamer<string> = hashFunc('sha256');
+export const SHA1: IWritableStreamer<string> = hashFunc('sha1');
+export const MD5: IWritableStreamer<string> = hashFunc('md5');
+
+
+/** In Source Testing */
+if (import.meta.vitest) {
+  const { describe, test, expect } = import.meta.vitest;
+  describe('hashGroup', async () => {
+    const { Bytes } = await import('./bytes');
+    const src = Bytes.fromString('test');
+
+    const testCases = [
+      {
+        algorithm: SHA256,
+        value:
+          '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',
+      },
+      {
+        algorithm: SHA1,
+        value: 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3',
+      },
+      {
+        algorithm: MD5,
+        value: '098f6bcd4621d373cade4e832627b4f6',
+      },
+    ];
+
+    test.each(testCases)('hash', async (tCase) => {
+      expect((await src.into(tCase.algorithm)).value).toBe(tCase.value);
+    });
+  });
+}
