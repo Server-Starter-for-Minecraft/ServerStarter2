32e0dc801f2a7cfc6c42db53c546d1da12999c5d
txkodo <kodo.hsmkai@gmail.com>
2024-05-13 23:47:31 +0900

    💡 streamにコメントを追加

--- src-electron/v2/util/binary/stream.ts
+++ src-electron/v2/util/binary/stream.ts
@@ -2,12 +2,41 @@ import * as stream from 'stream';
 import { Result } from '../base';
 
 export interface IReadableStreamer {
+  /**
+   * 読み込みストリームを生成する
+   *
+   * 基本的に into / convert から呼び出す目的
+   */
   createReadStream(): Readable;
+  /**
+   * ストリームを変換する
+   *
+   * e.g バイナリ から Base64 , ShiftJIS から utf8
+   *
+   * @param duplex ストリーム変換用オブジェクト 基本的にstream.Transform を想定
+   */
   convert(duplex: stream.Duplex): Readable;
+
+  /**
+   * ストリームを書き込む
+   *
+   * 書き込み先によっては 意味のある戻り値<T>を返すことがある
+   *
+   * メモリ上 / URL / パス / ハッシュ値 等
+   *
+   * @param target 書き込み先
+   */
   into<T>(target: IWritableStreamer<T>): Promise<Result<T, Error>>;
 }
 
 export interface IWritableStreamer<T> {
+  /**
+   * ストリームから書き込む
+   *
+   * 基本的に IReadableStreamer.into から呼び出されることを想定
+   *
+   * @param target 書き込み先
+   */
   write(readable: stream.Readable): Promise<Result<T, Error>>;
 }
 
