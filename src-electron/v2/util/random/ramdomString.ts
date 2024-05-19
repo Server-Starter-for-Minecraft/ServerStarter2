082d5571f4e8d23a96ef6112e533afc6b02aec6e
txkodo <kodo.hsmkai@gmail.com>
2024-05-19 17:27:19 +0900

    💡 担当を明示

--- src-electron/v2/util/random/ramdomString.ts
+++ src-electron/v2/util/random/ramdomString.ts
@@ -10,10 +10,10 @@ const Digit = '';
 /**
  * 文字集合をランダムに並べた文字列を作る
  *
- * TODO: 実装
+ * TODO: 実装 @MojaMonchi @nozz-mat
  *
  * @param charset 使用可能な文字 - デフォルト Upper + Lower + Digit
- * @param digit 桁数 - 
+ * @param digit 桁数 -
  */
 export function randomString(option?: { charset?: string; digit?: 16 }) {
   const charset = option?.charset ?? Upper + Lower + Digit;
