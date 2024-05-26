a22bdc31fe3c566cd28fef3af0f79af750b8a5f3
txkodo <kodo.hsmkai@gmail.com>
2024-05-26 16:57:26 +0900

    💡 runtime.tsにコメントを追加

--- src-electron/v2/core/world/runtime.ts
+++ src-electron/v2/core/world/runtime.ts
@@ -1,10 +1,13 @@
 import { RuntimeSettings } from '../../schema/runtime';
-import { Result, ok } from '../../util/base';
+import { ok, Result } from '../../util/base';
 
 /**
  * ランタイム設定を解析して、javaの引数 (string[]) に変換する
  *
  * 既存のコマンドライン引数パーサーがある場合そちらを使ったほうがいいかも
+ * これいいかも https://github.com/minimistjs/minimist
+ *
+ * 本当はutilに置くべきか
  *
  * e.g. "-Xms1024M -Xmx3072M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=256M" => ["-Xms1024M", "-Xmx3072M", "-XX:MetaspaceSize=128M", "-XX:MaxMetaspaceSize=256M"]
  *
