ff819fa37aa701c9527910221217dd72b06ab773
txkodo <kodo.hsmkai@gmail.com>
2024-05-26 17:09:08 +0900

    randmStringのテストを記述

--- src-electron/v2/util/random/ramdomString.ts
+++ src-electron/v2/util/random/ramdomString.ts
@@ -15,8 +15,26 @@ const Digit = '';
  * @param charset 使用可能な文字 - デフォルト Upper + Lower + Digit
  * @param digit 桁数 -
  */
-export function randomString(option?: { charset?: string; digit?: 16 }) {
+export function randomString(option?: { charset?: string; digit?: number }) {
   const charset = option?.charset ?? Upper + Lower + Digit;
   const digit = option?.digit ?? 16;
   return 'ABCDFRG';
+}
+
+/** In Source Testing */
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest;
+  test('randomString', () => {
+    // 引数なしで実行した場合 16 桁の A-Za-z0-9 からなる配列を返す
+    expect(randomString()).toMatch(/[A-Za-z0-9]{16}/);
+
+    // digit=10 で実行した場合 10 桁の A-Za-z0-9 からなる配列を返す
+    expect(randomString({ digit: 10 })).toMatch(/[A-Za-z0-9]{10}/);
+
+    // charset=abc 実行した場合 16 桁の abc からなる配列を返す
+    expect(randomString({ charset: 'abc' })).toMatch(/[abc]{16}/);
+
+    // digit=10 charset=abc で実行した場合 10 桁の abc からなる配列を返す
+    expect(randomString({ charset: 'abc', digit: 10 })).toMatch(/[abc]{16}/);
+  });
 }
