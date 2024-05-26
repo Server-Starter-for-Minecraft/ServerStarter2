c1abb50d58ac4965f324feae5c9873f302a13c0c
txkodo <kodo.hsmkai@gmail.com>
2024-05-26 17:17:26 +0900

    💡 コメントを更新

--- src-electron/v2/core/world/runtime.ts
+++ src-electron/v2/core/world/runtime.ts
@@ -4,11 +4,6 @@ import { ok, Result } from '../../util/base';
 /**
  * ランタイム設定を解析して、javaの引数 (string[]) に変換する
  *
- * 既存のコマンドライン引数パーサーがある場合そちらを使ったほうがいいかも
- * これいいかも https://github.com/minimistjs/minimist
- *
- * 本当はutilに置くべきか
- *
  * e.g. "-Xms1024M -Xmx3072M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=256M" => ["-Xms1024M", "-Xmx3072M", "-XX:MetaspaceSize=128M", "-XX:MaxMetaspaceSize=256M"]
  *
  * TODO: @MojaMonchi @nozz-mat
