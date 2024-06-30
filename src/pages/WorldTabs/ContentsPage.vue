ec074edc04aa26f880a39b8878423a4a324b81f2
CivilTT <CivilTT@users.noreply.github.com>
2024-06-30 03:04:04 +0000

    🚨 Apply Prettier Change

--- src/pages/WorldTabs/ContentsPage.vue
+++ src/pages/WorldTabs/ContentsPage.vue
@@ -8,18 +8,22 @@ const mainStore = useMainStore();
 const contentsStore = useContentsStore();
 
 // CardViewは廃止予定のため，表示させない
-const cardViewSelector = false
+const cardViewSelector = false;
 </script>
 
 <template>
   <q-scroll-area class="full-height" style="flex: 1 1 0">
     <CardView
       v-if="mainStore.world && cardViewSelector"
-      :content-type="contentsStore.getShowingContentPage(mainStore.world.version.type)"
+      :content-type="
+        contentsStore.getShowingContentPage(mainStore.world.version.type)
+      "
     />
     <ListView
       v-if="mainStore.world && !cardViewSelector"
-      :content-type="contentsStore.getShowingContentPage(mainStore.world.version.type)"
+      :content-type="
+        contentsStore.getShowingContentPage(mainStore.world.version.type)
+      "
     />
   </q-scroll-area>
 </template>
