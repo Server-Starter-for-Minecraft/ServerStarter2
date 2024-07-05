737b2d565f7d5df994026976da226bf294131664
CivilTT <civiltt.ss@gmail.com>
2024-07-06 02:05:36 +0900

    ✨ Added jar setter in vanilla.ts

--- src-electron/v2/source/version/fileProcess/vanilla.ts
+++ src-electron/v2/source/version/fileProcess/vanilla.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { serverSourcePath } from 'app/src-electron/v2/core/const';
 import { minecraftRuntimeVersions } from 'app/src-electron/v2/schema/runtime';
 import {
   VanillaVersion,
@@ -7,16 +8,20 @@ import {
 } from 'app/src-electron/v2/schema/version';
 import { err, ok, Result } from 'app/src-electron/v2/util/base';
 import { Bytes } from 'app/src-electron/v2/util/binary/bytes';
+import { Path } from 'app/src-electron/v2/util/binary/path';
 import { Url } from 'app/src-electron/v2/util/binary/url';
 import { JsonSourceHandler } from 'app/src-electron/v2/util/wrapper/jsonFile';
 import {
   getCacheVerFolderPath,
+  getJarPath,
   ServerVersionFileProcess,
 } from '../fileProcess/base';
 import { getVersionMainfest } from '../getVersions/mainfest';
 import {
   generateVersionJsonHandler,
   getVersionJsonObj,
+  getVersionJsonPath,
+  replaceEmbedArgs,
   VersionJson,
 } from './versionJson';
 
@@ -29,7 +34,8 @@ const verJsonHandlers: { [vId in VersionId]: JsonSourceHandler<VersionJson> } =
 function vanillaMetaInfo2VersionJson(
   version: Version,
   metaObj: Record<string, any>
-) {
+): Promise<Result<VersionJson>> {
+  // 取得したJSONのパース用型定義
   const metaInfoZod = z.object({
     downloads: z.object({
       server: z.object({
@@ -50,7 +56,7 @@ function vanillaMetaInfo2VersionJson(
     .transform((obj) =>
       getVersionJsonObj(version, obj.downloads.server.url, obj.javaVersion)
     )
-    .parse(metaObj);
+    .parseAsync(metaObj);
 }
 
 export function getVanillaFp(): ServerVersionFileProcess<VanillaVersion> {
@@ -67,8 +73,12 @@ export function getVanillaFp(): ServerVersionFileProcess<VanillaVersion> {
         return verJson;
       }
 
-      // TODO: `server.jar`や`libraries`を取得し，要求されたパスに配置する
-      // `server.jar`や`libraries`がキャッシュに存在しないときはDLし，キャッシュにもDLしたJarを配置する
+      // `server.jar`や`libraries`を取得し，要求されたパスに配置する
+      const jarRes = await setJar(cacheDir, path, verJson.value());
+      if (jarRes.isErr) {
+        console.log('test');
+        return jarRes;
+      }
 
       return ok({
         runtime: {
@@ -76,9 +86,10 @@ export function getVanillaFp(): ServerVersionFileProcess<VanillaVersion> {
           version: verJson.value().javaVersion?.component ?? 'jre-legacy',
         },
         getCommand: (option: { jvmArgs: string[] }) => {
-          // TODO: { "embed": "JVM_ARGUMENT" }をjvmArgsで置換したうえで，最終的なArgsを返す
-          // 置換処理は`versionJson.ts`で一般化すると良い
-          return [];
+          return replaceEmbedArgs(verJson.value().arguments, {
+            JAR_PATH: [getVersionJsonPath(path).toStr()],
+            JVM_ARGUMENT: option.jvmArgs,
+          });
         },
       });
     },
@@ -97,7 +108,7 @@ async function getVersionJson(
   version: VanillaVersion
 ): Promise<Result<VersionJson>> {
   // Handlerを生成して登録する
-  if (Object.keys(verJsonHandlers).some((vId) => vId === version.id)) {
+  if (!Object.keys(verJsonHandlers).some((vId) => vId === version.id)) {
     verJsonHandlers[version.id] = generateVersionJsonHandler(
       version.type,
       version.id
@@ -133,7 +144,7 @@ async function getVersionJson(
   }
 
   // JSON情報を変換して`version.json`に書き込めるオブジェクトを生成
-  const verJson = vanillaMetaInfo2VersionJson(
+  const verJson = await vanillaMetaInfo2VersionJson(
     version,
     JSON.parse(jsonStr.value())
   );
@@ -143,4 +154,97 @@ async function getVersionJson(
 
   verJsonHandlers[version.id].write(verJson.value());
   return verJson;
+}
+
+/**
+ * Jarファイルを所定の位置にセットする
+ *
+ * Jarのキャッシュがない場合はダウンロードデータをセットする
+ */
+async function setJar(
+  cachePath: Path,
+  targetPath: Path,
+  info: VersionJson
+): Promise<Result<void>> {
+  const cacheJarPath = getJarPath(cachePath);
+  const cachelibPath = cachePath.child('libraries');
+  const cacheEulaPath = cachePath.child('eula.txt');
+  // Jarをセット
+  if (cacheJarPath.exists()) {
+    await cacheJarPath.copyTo(getJarPath(targetPath));
+    // libs, eulaはあってもなくても良いため，チェックせずにコピーしようとさせる
+    await cachelibPath.copyTo(targetPath.child('libraries'));
+    await cacheEulaPath.copyTo(targetPath.child('eula.txt'));
+  } else {
+    const downloadJar = await new Url(info.download.url).into(Bytes);
+    if (downloadJar.isErr) {
+      return downloadJar;
+    }
+
+    // ダウンロードしたJarデータを書き出し
+    const jarPath = getJarPath(targetPath);
+    await jarPath.mkdir();
+    return await downloadJar.value().into(jarPath);
+  }
+
+  return ok();
+}
+
+/** In Source Testing */
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest;
+
+  const ver21: VanillaVersion = {
+    id: '1.21' as VersionId,
+    type: 'vanilla',
+    release: true,
+  };
+
+  test('metaJsonParser', async () => {
+    const urlFromManifest =
+      'https://piston-meta.mojang.com/v1/packages/177e49d3233cb6eac42f0495c0a48e719870c2ae/1.21.json';
+
+    const jsonStr = (await new Url(urlFromManifest).into(Bytes)).onOk((val) =>
+      val.toStr()
+    );
+    if (jsonStr.isErr) {
+      return jsonStr;
+    }
+
+    // JSON情報を変換して`version.json`に書き込めるオブジェクトを生成
+    const verJson = await vanillaMetaInfo2VersionJson(
+      ver21,
+      JSON.parse(jsonStr.value())
+    );
+
+    expect(verJson.isOk).toBe(true);
+    expect(verJson.value().javaVersion?.component).toEqual(
+      'java-runtime-delta'
+    );
+  });
+
+  test('setVersionJar', async () => {
+    const outputPath = serverSourcePath.child('test/ver21');
+    const cachePath = getCacheVerFolderPath(ver21);
+
+    // 条件をそろえるために，ファイル類を削除する
+    await outputPath.remove();
+    await cachePath?.remove();
+
+    const fp = getVanillaFp();
+    const res = await fp.setVersionFile(ver21, outputPath, async (runtime) =>
+      ok()
+    );
+
+    // 戻り値の検証
+    expect(res.isOk).toBe(true);
+    expect(res.value().getCommand({ jvmArgs: ['replaceArg'] })[0]).toBe(
+      'replaceArg'
+    );
+
+    // ファイルの設置状況の検証
+    expect(getJarPath(outputPath).exists()).toBe(true);
+    // Jarを実行しないと生成されないため，今回はTestの対象外
+    // expect(outputPath.child('libraries').exists()).toBe(true);
+  });
 }
