9b1e951d65fffd006773ba019c95a3c42df627b1
txkodo <kodo.hsmkai@gmail.com>
2024-07-13 23:26:31 +0900

    🚧 localワールドの読み込みを作成

--- src-electron/v2/source/world/local.ts
+++ src-electron/v2/source/world/local.ts
@@ -1,21 +1,53 @@
 import { World, WorldLocation, WorldName } from '../../schema/world';
-import { err, Result } from '../../util/base';
+import { err, ok, Result } from '../../util/base';
+import { Json } from '../../util/binary/json';
 import { Path } from '../../util/binary/path';
 import { InfinitMap } from '../../util/helper/infinitMap';
+import { AsyncCache } from '../../util/promise/cache';
 import { WorldContainerHandler } from './container';
+
+const SETTING_FILE_NAME = 'server_settings.json';
 
 /**
  * ローカルのワールドを管理するクラス
  */
 export class LocalWorldSource implements WorldContainerHandler {
   private dirpath: Path;
-  worldMataMap: any;
+  private worldMataMap: InfinitMap<WorldName, AsyncCache<World>>;
 
   constructor(dirpath: Path) {
     this.dirpath = dirpath;
-    this.worldMataMap = InfinitMap.primitiveKeyWeakValue<WorldName, any>(
-      (k) => {}
+
+    const settingJson = new Json(World);
+
+    const getWorldMeta = async (
+      worldName: WorldName
+    ): Promise<Result<World>> => {
+      // TODO: 読み込み失敗時にデフォルト値を使用?
+      const data = await this.settingFilePath(worldName).into(settingJson);
+      return data;
+    };
+
+    const setWorldMeta = async (
+      worldName: WorldName,
+      world: World
+    ): Promise<Result<World>> => {
+      settingJson.data = ok(world);
+      const setResult = await settingJson.into(this.settingFilePath(worldName));
+      return setResult.onOk(() => ok(world));
+    };
+
+    this.worldMataMap = InfinitMap.primitiveKeyWeakValue(
+      (worldName: WorldName) =>
+        new AsyncCache<World>(
+          () => getWorldMeta(worldName),
+          (world) => setWorldMeta(worldName, world)
+        )
     );
+  }
+
+  private settingFilePath(worldName: WorldName): Path {
+    return this.dirpath.child(worldName).child(SETTING_FILE_NAME);
   }
 
   async listWorldLocations(): Promise<WorldLocation[]> {
@@ -40,17 +72,25 @@ export class LocalWorldSource implements WorldContainerHandler {
   }
 
   setWorldMeta(name: WorldName, world: World): Promise<Result<void>> {
-    throw new Error('Method not implemented.');
+    return this.worldMataMap.get(name).set(world);
   }
+
   getWorldMeta(name: WorldName): Promise<Result<World>> {
-    throw new Error('Method not implemented.');
+    return this.worldMataMap.get(name).get();
   }
-  deleteWorldData(name: WorldName): Promise<Result<void>> {
-    throw new Error('Method not implemented.');
+
+  async deleteWorldData(name: WorldName): Promise<Result<void>> {
+    return ok(await this.dirpath.child(name).remove());
   }
-  extractWorldData(name: WorldName): Promise<Result<Path>> {
-    throw new Error('Method not implemented.');
+
+  async extractWorldData(name: WorldName): Promise<Result<Path>> {
+    const meta = await this.getWorldMeta(name);
+    if (meta.isErr) return meta;
+    const world = meta.value();
+
+    // TODO: ここでデータを展開
   }
+
   packWorldData(name: WorldName): Promise<Result<void>> {
     throw new Error('Method not implemented.');
   }
