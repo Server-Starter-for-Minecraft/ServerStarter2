f9226b06a87365540bc8cf53437cb33bb014981b
txkodo <kodo.hsmkai@gmail.com>
2024-07-28 03:59:47 +0900

    🚧 🏷️OuterContainer.fetchUniversalRuntimeMapの型定義を作成中

--- src-electron/v2/source/outer/ourer.ts
+++ src-electron/v2/source/outer/ourer.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+import { Result } from '../../util/base';
 import { Path } from '../../util/binary/path';
 
 /** OuterResourceリポジトリにアクセスしてデータを取得するためのクラス */
@@ -6,4 +8,22 @@ export class OuterContainer {
   constructor(cacheDirPath: Path) {
     this.cacheDirPath = cacheDirPath;
   }
+
+  async fetchUniversalRuntimeMap(): Promise<Result<UniversalRuntimeMap>> {
+    throw new Error('未実装');
+  }
 }
+
+export const UniversalRuntimeMap = z.object({
+  versions: z.record(
+    z.string() /** TODO: z.string()をJavaMajorVersionに変更 */,
+    z.record(
+      z.enum(['windows', 'mac', 'linux']),
+      z.record(
+        z.enum(['x-64', 'arm-64'])
+        /** TODO: valueの型をRuntimeにする */
+      )
+    )
+  ),
+});
+export type UniversalRuntimeMap = z.infer<typeof UniversalRuntimeMap>;
