8a52cb95b983d19d9b4f3f05a7c1ef3c6288971d
CivilTT <civiltt.ss@gmail.com>
2024-08-04 14:24:05 +0900

    ⏪️ Reverted invalid settings

--- vitest.config.js
+++ vitest.config.js
@@ -6,9 +6,6 @@ export default defineConfig({
     include: ['**/*.test.ts'],
     includeSource: ['**/*.ts'],
     globals: true,
-    reporters: process.env.GITHUB_ACTIONS
-      ? ['basic', 'github-actions']
-      : ['basic'],
     coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
