099bf133e0ede9d9ab1aaf7f601a6eb8372acf0b
CivilTT <civiltt.ss@gmail.com>
2024-08-04 11:22:11 +0900

    🩹 Fixed vitest configs relative to upgrade the vitest version

--- vitest.config.js
+++ vitest.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     includeSource: ['**/*.ts'],
     globals: true,
     coverage: {
+      provider: 'istanbul', // or 'v8'
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
       // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
