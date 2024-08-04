7e56dfb56767d91914e667fc2114e0ef893461c3
CivilTT <civiltt.ss@gmail.com>
2024-08-04 10:59:09 +0900

    👷 Added vitest's workflow

--- vitest.config.js
+++ vitest.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
     include: ['**/*.test.ts'],
     includeSource: ['**/*.ts'],
     globals: true,
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
   resolve: {
     alias: {
