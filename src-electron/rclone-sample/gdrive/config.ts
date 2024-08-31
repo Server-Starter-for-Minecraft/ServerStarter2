ae4ebb4d1ade8836250f0dee91a4cddf4414822f
nozz-mat <minaharu318@gmail.com>
2024-08-31 23:43:12 +0900

    🩹 google drive用のコードをフォルダに集約

---
+++ src-electron/rclone-sample/gdrive/config.ts
@@ -0,0 +1,16 @@
+import { ChildProcess } from 'child_process';
+import { config } from 'rclone.js';
+
+/** In Source Testing */
+if (import.meta.vitest) {
+  const { test, expect } = import.meta.vitest;
+  test('makeGoogleConfig',async () => {
+    const makeConfigProcess = config('create','gdriveMake','drive',{
+      env: {
+        RCLONE_CONFIG: 'src-electron/rclone-sample/rclone.conf',
+      },
+    });
+    //await new Promise<void>((r) => makeConfigProcess.on('close', r));
+
+  },50000)
+}
