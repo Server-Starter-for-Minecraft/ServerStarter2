2ac36f3ef699ab115ac687f8ae6679927821fa7c
CivilTT <civiltt.ss@gmail.com>
2025-01-27 22:27:32 +0900

    🔧 Added coverage settings in `vitest.config.js`

--- vitest.config.js
+++ vitest.config.js
@@ -6,6 +6,28 @@ export default defineConfig({
     include: ['**/*.test.ts'],
     includeSource: ['**/*.ts'],
     globals: true,
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+      // target is only backend files
+      include: ['**/src-electron/**'],
+      // removed no test files
+      exclude: [
+        'src-electron/*.ts',
+        '**/schema/**',
+        '**/api/**',
+        '**/dummy/**',
+      ],
+      // set limit ratio
+      // thresholds: {
+      //   lines: 60,
+      //   branches: 60,
+      //   functions: 60,
+      //   statements: 60,
+      // },
+    },
   },
   resolve: {
     alias: {
