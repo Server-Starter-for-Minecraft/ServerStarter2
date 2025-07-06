88ca7af648689042599179af2ec17e9274da90ae
CivilTT <civiltt.ss@gmail.com>
2025-07-06 17:30:45 +0900

    🩹 Fixed the splitter's limit width in ListView

--- src/components/World/Player/ListView.vue
+++ src/components/World/Player/ListView.vue
@@ -43,7 +43,7 @@ const splitPos = ref(DEFAULT_POS);
     <q-splitter
       v-model="splitPos"
       @dblclick="splitPos = DEFAULT_POS"
-      :limits="[15, 80]"
+      :limits="[15, 65]"
       emit-immediately
       separator-style="margin-left: 10px; margin-right: 10px"
       class="q-py-md fit col"
